830efa4f14c7686a7e5566c153f9adf1bb2fa540
bvanseg <bvanseg@gmail.com>
2024-04-12 04:30:41 -0400

    Terry A. Davis would not approve.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/client/render/entity/living/SpitterRenderer.java
+++ common/src/main/java/org/avp/client/render/entity/living/SpitterRenderer.java
@@ -4,11 +4,13 @@ import mod.azure.azurelib.common.api.client.renderer.GeoEntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 
 import org.avp.client.model.entity.living.SpitterModel;
+import org.avp.client.render.entity.living.layer.SpitterGlowLayer;
 import org.avp.common.entity.living.Spitter;
 
 public class SpitterRenderer extends GeoEntityRenderer<Spitter> {
 
     public SpitterRenderer(EntityRendererProvider.Context context) {
         super(context, new SpitterModel());
+        this.addRenderLayer(new SpitterGlowLayer(this));
     }
 }
