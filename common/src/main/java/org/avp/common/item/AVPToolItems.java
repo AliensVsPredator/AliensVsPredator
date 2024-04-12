cca03de6010bc648d8c6dabbd530f3b8106caccb
bvanseg <bvanseg@gmail.com>
2024-04-12 03:22:15 -0400

    Added titanium tool items.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/common/item/AVPToolItems.java
+++ common/src/main/java/org/avp/common/item/AVPToolItems.java
@@ -13,6 +13,16 @@ public class AVPToolItems {
 
     private static final List<GameObject<Item>> ENTRIES = new ArrayList<>();
 
+    public static final GameObject<Item> TITANIUM_AXE;
+
+    public static final GameObject<Item> TITANIUM_HOE;
+
+    public static final GameObject<Item> TITANIUM_PICKAXE;
+
+    public static final GameObject<Item> TITANIUM_SHOVEL;
+
+    public static final GameObject<Item> TITANIUM_SWORD;
+
     public static final GameObject<Item> VERITANIUM_AXE;
 
     public static final GameObject<Item> VERITANIUM_HOE;
@@ -31,8 +41,12 @@ public class AVPToolItems {
         return ENTRIES;
     }
 
+    private static GameObject<Item> register(String registryName) {
+        return register(registryName, () -> new Item(new Item.Properties()));
+    }
+
     private static GameObject<Item> register(String registryName, Supplier<Item> itemSupplier) {
-        var gameObject = Services.ITEM_REGISTRY.register(registryName, itemSupplier);
+        var gameObject = Services.ITEM_REGISTRY.register("tool_" + registryName, itemSupplier);
         ENTRIES.add(gameObject);
         return gameObject;
     }
@@ -40,10 +54,15 @@ public class AVPToolItems {
     private AVPToolItems() {}
 
     static {
-        VERITANIUM_AXE = register("tool_veritanium_axe", () -> new Item(new Item.Properties()));
-        VERITANIUM_HOE = register("tool_veritanium_hoe", () -> new Item(new Item.Properties()));
-        VERITANIUM_PICKAXE = register("tool_veritanium_pickaxe", () -> new Item(new Item.Properties()));
-        VERITANIUM_SHOVEL = register("tool_veritanium_shovel", () -> new Item(new Item.Properties()));
-        VERITANIUM_SWORD = register("tool_veritanium_sword", () -> new Item(new Item.Properties()));
+        TITANIUM_AXE = register("titanium_axe");
+        TITANIUM_HOE = register("titanium_hoe");
+        TITANIUM_PICKAXE = register("titanium_pickaxe");
+        TITANIUM_SHOVEL = register("titanium_shovel");
+        TITANIUM_SWORD = register("titanium_sword");
+        VERITANIUM_AXE = register("veritanium_axe");
+        VERITANIUM_HOE = register("veritanium_hoe");
+        VERITANIUM_PICKAXE = register("veritanium_pickaxe");
+        VERITANIUM_SHOVEL = register("veritanium_shovel");
+        VERITANIUM_SWORD = register("veritanium_sword");
     }
 }
