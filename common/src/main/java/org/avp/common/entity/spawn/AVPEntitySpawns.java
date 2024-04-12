32dde3d49af26ac3b9f6ede9f111dd164bc9e3b5
bvanseg <bvanseg@gmail.com>
2024-04-12 02:59:19 -0400

    Xenomorphs now spawn based on Y level.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/common/entity/spawn/AVPEntitySpawns.java
+++ common/src/main/java/org/avp/common/entity/spawn/AVPEntitySpawns.java
@@ -1,12 +1,17 @@
 package org.avp.common.entity.spawn;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.levelgen.Heightmap;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import org.avp.api.GameObject;
 import org.avp.common.entity.AVPBaseAlienEntityTypes;
@@ -27,6 +32,7 @@ public class AVPEntitySpawns {
 
     private static <T extends Monster> void registerMonsterSpawn(
         GameObject<EntityType<T>> entityTypeGameObject,
+        SpawnPlacements.SpawnPredicate<T> spawnPredicate,
         int weight,
         int minGroupSize,
         int maxGroupSize
@@ -39,24 +45,41 @@ public class AVPEntitySpawns {
                 maxGroupSize,
                 SpawnPlacements.Type.ON_GROUND,
                 Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Monster::checkMonsterSpawnRules
+                spawnPredicate
             )
         );
     }
 
+    private static <T extends Monster> void registerAlienSpawn(
+        GameObject<EntityType<T>> entityTypeGameObject,
+        SpawnPlacements.SpawnPredicate<T> spawnPredicate,
+        int maxYLevel,
+        int weight,
+        int minGroupSize,
+        int maxGroupSize
+    ) {
+        registerMonsterSpawn(
+            entityTypeGameObject,
+            (e, s, m, b, r) -> b.getY() <= maxYLevel && spawnPredicate.test(e, s, m, b, r),
+            weight,
+            minGroupSize,
+            maxGroupSize
+        );
+    }
+
     static {
-        registerMonsterSpawn(AVPBaseAlienEntityTypes.BOILER, 7, 1, 1);
-        registerMonsterSpawn(AVPBaseAlienEntityTypes.DRONE, 50, 1, 2);
-        registerMonsterSpawn(AVPBaseAlienEntityTypes.PRAETORIAN, 10, 1, 1);
-        registerMonsterSpawn(AVPBaseAlienEntityTypes.QUEEN, 5, 1, 1);
-        registerMonsterSpawn(AVPBaseAlienEntityTypes.SPITTER, 10, 1, 1);
-        registerMonsterSpawn(AVPBaseAlienEntityTypes.WARRIOR, 25, 1, 1);
+        registerAlienSpawn(AVPBaseAlienEntityTypes.BOILER, Monster::checkMonsterSpawnRules, 0, 7, 1, 1);
+        registerAlienSpawn(AVPBaseAlienEntityTypes.DRONE, Monster::checkMonsterSpawnRules, 75, 50, 1, 2);
+        registerAlienSpawn(AVPBaseAlienEntityTypes.PRAETORIAN, Monster::checkMonsterSpawnRules, 0, 10, 1, 1);
+        registerAlienSpawn(AVPBaseAlienEntityTypes.QUEEN, Monster::checkMonsterSpawnRules, -24, 5, 1, 1);
+        registerAlienSpawn(AVPBaseAlienEntityTypes.SPITTER, Monster::checkMonsterSpawnRules, 0, 10, 1, 1);
+        registerAlienSpawn(AVPBaseAlienEntityTypes.WARRIOR, Monster::checkMonsterSpawnRules, 32, 25, 1, 1);
 
-        registerMonsterSpawn(AVPRunnerAlienEntityTypes.DRONE_RUNNER, 50, 1, 3);
-        registerMonsterSpawn(AVPRunnerAlienEntityTypes.WARRIOR_RUNNER, 25, 1, 2);
-        registerMonsterSpawn(AVPRunnerAlienEntityTypes.CRUSHER, 10, 1, 1);
+        registerAlienSpawn(AVPRunnerAlienEntityTypes.DRONE_RUNNER, Monster::checkMonsterSpawnRules, 75, 50, 1, 3);
+        registerAlienSpawn(AVPRunnerAlienEntityTypes.WARRIOR_RUNNER, Monster::checkMonsterSpawnRules, 32, 25, 1, 2);
+        registerAlienSpawn(AVPRunnerAlienEntityTypes.CRUSHER, Monster::checkMonsterSpawnRules, 0, 10, 1, 1);
 
-        registerMonsterSpawn(AVPYautjaEntityTypes.YAUTJA, 1, 1, 1);
+        registerMonsterSpawn(AVPYautjaEntityTypes.YAUTJA, Monster::checkMonsterSpawnRules, 1, 1, 1);
     }
 
     private AVPEntitySpawns() {
