992589aab67ccdeaa7ff522865e29d1e282c2e73
bvanseg <bvanseg@gmail.com>
2024-04-13 15:16:30 -0400

    Fixed base alien hitboxes.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/common/entity/AVPBaseAlienEntityTypes.java
+++ common/src/main/java/org/avp/common/entity/AVPBaseAlienEntityTypes.java
@@ -20,6 +20,7 @@ public class AVPBaseAlienEntityTypes implements AVPRegistry {
         0x010202,
         0x9DA930,
         EntityType.Builder.of(Boiler::new, MobCategory.MONSTER)
+            .sized(0.98F, 2.48F)
     );
 
     public static final GameObject<EntityType<Chestburster>> CHESTBURSTER = AVPEntityTypes.registerLiving(
@@ -27,6 +28,7 @@ public class AVPBaseAlienEntityTypes implements AVPRegistry {
         0xD8B877,
         0xF7E2B4,
         EntityType.Builder.of(Chestburster::new, MobCategory.MONSTER)
+            .sized(0.75F, 0.3F)
     );
 
     public static final GameObject<EntityType<Drone>> DRONE = AVPEntityTypes.registerLiving(
@@ -34,6 +36,7 @@ public class AVPBaseAlienEntityTypes implements AVPRegistry {
         0x010202,
         0xDFE2E4,
         EntityType.Builder.of(Drone::new, MobCategory.MONSTER)
+            .sized(0.98F, 1.98F)
     );
 
     public static final GameObject<EntityType<Facehugger>> FACEHUGGER = AVPEntityTypes.registerLiving(
@@ -41,6 +44,7 @@ public class AVPBaseAlienEntityTypes implements AVPRegistry {
         0xE4D597,
         0xA55863,
         EntityType.Builder.of(Facehugger::new, MobCategory.MONSTER)
+            .sized(0.75F, 0.3F)
     );
 
     public static final GameObject<EntityType<FacehuggerRoyal>> FACEHUGGER_ROYAL = AVPEntityTypes.registerLiving(
@@ -48,6 +52,7 @@ public class AVPBaseAlienEntityTypes implements AVPRegistry {
         0x81785E,
         0x583A3A,
         EntityType.Builder.of(FacehuggerRoyal::new, MobCategory.MONSTER)
+            .sized(0.75F, 0.3F)
     );
 
     public static final GameObject<EntityType<Ovamorph>> OVAMORPH = AVPEntityTypes.registerLiving(
@@ -55,6 +60,7 @@ public class AVPBaseAlienEntityTypes implements AVPRegistry {
         0x615B45,
         0xBF7872,
         EntityType.Builder.of(Ovamorph::new, MobCategory.MONSTER)
+            .sized(0.88F, 0.98F)
     );
 
     public static final GameObject<EntityType<Praetorian>> PRAETORIAN = AVPEntityTypes.registerLiving(
@@ -62,6 +68,7 @@ public class AVPBaseAlienEntityTypes implements AVPRegistry {
         0x010202,
         0x363534,
         EntityType.Builder.of(Praetorian::new, MobCategory.MONSTER)
+            .sized(0.98F, 2.48F)
     );
 
     public static final GameObject<EntityType<Queen>> QUEEN = AVPEntityTypes.registerLiving(
@@ -69,6 +76,7 @@ public class AVPBaseAlienEntityTypes implements AVPRegistry {
         0x010202,
         0x363534,
         EntityType.Builder.of(Queen::new, MobCategory.MONSTER)
+            .sized(1.98F, 3.98F)
     );
 
     public static final GameObject<EntityType<Spitter>> SPITTER = AVPEntityTypes.registerLiving(
@@ -76,6 +84,7 @@ public class AVPBaseAlienEntityTypes implements AVPRegistry {
         0x010202,
         0x3CDC09,
         EntityType.Builder.of(Spitter::new, MobCategory.MONSTER)
+            .sized(0.98F, 1.98F)
     );
 
     public static final GameObject<EntityType<Warrior>> WARRIOR = AVPEntityTypes.registerLiving(
@@ -83,6 +92,7 @@ public class AVPBaseAlienEntityTypes implements AVPRegistry {
         0x010202,
         0x4A4E55,
         EntityType.Builder.of(Warrior::new, MobCategory.MONSTER)
+            .sized(0.98F, 1.98F)
     );
 
     @Override
