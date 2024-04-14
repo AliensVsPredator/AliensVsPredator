90f759132b7b756391117ab83b9ed2a896347d0b
bvanseg <bvanseg@gmail.com>
2024-04-13 20:38:07 -0400

    Fixed exotic alien hitboxes.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/common/entity/AVPExoticAlienEntityTypes.java
+++ common/src/main/java/org/avp/common/entity/AVPExoticAlienEntityTypes.java
@@ -21,6 +21,7 @@ public class AVPExoticAlienEntityTypes implements AVPRegistry {
             0x8896A5,
             0x495256,
             EntityType.Builder.of(DeaconAdultEngineer::new, MobCategory.MONSTER)
+                .sized(0.98F, 2.98F)
         );
 
     public static final GameObject<EntityType<Dracoburster>> DRACOBURSTER = AVPEntityTypes.registerLiving(
@@ -28,6 +29,7 @@ public class AVPExoticAlienEntityTypes implements AVPRegistry {
         0xD8B877,
         0xF7E2B4,
         EntityType.Builder.of(Dracoburster::new, MobCategory.MONSTER)
+            .sized(0.98F, 1.98F)
     );
 
     public static final GameObject<EntityType<Dracomorph>> DRACOMORPH = AVPEntityTypes.registerLiving(
@@ -35,6 +37,7 @@ public class AVPExoticAlienEntityTypes implements AVPRegistry {
         0x212121,
         0x535353,
         EntityType.Builder.of(Dracomorph::new, MobCategory.MONSTER)
+            .sized(1.98F, 2.98F)
     );
 
     public static final GameObject<EntityType<Octohugger>> OCTOHUGGER = AVPEntityTypes.registerLiving(
@@ -42,6 +45,7 @@ public class AVPExoticAlienEntityTypes implements AVPRegistry {
         0xC2BCC8,
         0xC09CAE,
         EntityType.Builder.of(Octohugger::new, MobCategory.MONSTER)
+            .sized(0.25F, 0.75F)
     );
 
     public static final GameObject<EntityType<OvamorphDraco>> OVAMORPH_DRACO = AVPEntityTypes.registerLiving(
@@ -49,6 +53,7 @@ public class AVPExoticAlienEntityTypes implements AVPRegistry {
         0x2F2F2F,
         0xA36762,
         EntityType.Builder.of(OvamorphDraco::new, MobCategory.MONSTER)
+            .sized(0.98F, 0.98F)
     );
 
     public static final GameObject<EntityType<Ultramorph>> ULTRAMORPH = AVPEntityTypes.registerLiving(
@@ -56,6 +61,7 @@ public class AVPExoticAlienEntityTypes implements AVPRegistry {
         0x3E474E,
         0x696E76,
         EntityType.Builder.of(Ultramorph::new, MobCategory.MONSTER)
+            .sized(0.98F, 1.98F)
     );
 
     @Override
