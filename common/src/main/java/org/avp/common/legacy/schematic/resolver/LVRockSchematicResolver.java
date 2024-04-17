4ff56342c5ccee21326e1c8adbec8cc914af1a81
bvanseg <bvanseg@gmail.com>
2024-04-17 17:50:33 -0400

    Resolved TODO.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/common/legacy/schematic/resolver/LVRockSchematicResolver.java
+++ common/src/main/java/org/avp/common/legacy/schematic/resolver/LVRockSchematicResolver.java
@@ -2,6 +2,7 @@ package org.avp.common.legacy.schematic.resolver;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import org.avp.common.block.AVPUnidentifiedBlocks;
 
 import java.util.Map;
 
@@ -9,7 +10,7 @@ public class LVRockSchematicResolver {
 
     public static final Map<String, Block> RESOLVER_MAP = Map.<String, Block>ofEntries(
         Map.entry("0", Blocks.AIR),
-        Map.entry("1", Blocks.WHITE_WOOL) // TODO: LV 426 Stone
+        Map.entry("1", AVPUnidentifiedBlocks.INSTANCE.rock.get())
     );
 
     private LVRockSchematicResolver() {
