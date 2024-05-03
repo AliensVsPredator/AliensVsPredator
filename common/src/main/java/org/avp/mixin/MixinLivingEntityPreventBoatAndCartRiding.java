ccc35cb9dccfe62f6a7ecfc3604e9b09d85498af
bvanseg <bvanseg@gmail.com>
2024-05-02 23:37:23 -0400

    Fixed aliens/predators getting trapped by boats/minecarts.
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ common/src/main/java/org/avp/mixin/MixinLivingEntityPreventBoatAndCartRiding.java
@@ -0,0 +1,49 @@
+package org.avp.mixin;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.vehicle.Boat;
+import net.minecraft.world.entity.vehicle.Minecart;
+import org.avp.common.tag.AVPEntityTags;
+import org.avp.common.util.MixinUtils;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public abstract class MixinLivingEntityPreventBoatAndCartRiding {
+
+    // If a boat or cart attempts to force the alien/predator to start riding it, tell the boat no.
+    @Inject(at = @At("HEAD"), method = "startRiding", cancellable = true)
+    void startRiding(Entity entity, CallbackInfoReturnable<Boolean> callbackInfoReturnable) {
+        var self = MixinUtils.<Entity>self(this);
+
+        // All aliens will not ride boats.
+        if (
+            !self.getType().is(AVPEntityTags.ALIENS) &&
+            !self.getType().is(AVPEntityTags.PREDATORS)
+        ) return;
+
+        if (entity instanceof Boat || entity instanceof Minecart) {
+            callbackInfoReturnable.setReturnValue(false);
+        }
+    }
+
+    // If the alien/predator is already riding a boat somehow, stop riding the boat.
+    @Inject(at = @At("HEAD"), method = "tick")
+    void tick(CallbackInfo callbackInfo) {
+        var self = MixinUtils.<Entity>self(this);
+        var level = self.level();
+
+        if (level.isClientSide) return;
+        if (
+            !self.getType().is(AVPEntityTags.ALIENS) &&
+            !self.getType().is(AVPEntityTags.PREDATORS)
+        ) return;
+
+        if (self.getVehicle() instanceof Boat || self.getVehicle() instanceof Minecart) {
+            self.stopRiding();
+        }
+    }
+}
