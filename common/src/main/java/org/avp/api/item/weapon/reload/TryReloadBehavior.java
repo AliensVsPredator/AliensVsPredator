ad242b429f286c4bb19e6b7fb8b7b16acabdee7e
bvanseg <bvanseg@gmail.com>
2024-05-05 09:06:03 -0400

    Ammunition can now be consumed directly from shulker boxes.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/api/item/weapon/reload/TryReloadBehavior.java
+++ common/src/main/java/org/avp/api/item/weapon/reload/TryReloadBehavior.java
@@ -1,21 +1,25 @@
 package org.avp.api.item.weapon.reload;
 
+import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-
-import java.time.Duration;
-import java.util.Objects;
-
 import org.avp.api.item.weapon.WeaponItemData;
 import org.avp.api.item.weapon.WeaponItemTagHelper;
 import org.avp.common.util.AVPPredicates;
 import org.avp.server.ServerScheduler;
 
+import java.time.Duration;
+import java.util.Objects;
+
 @FunctionalInterface
 public interface TryReloadBehavior {
+
+    String BLOCK_ENTITY_TAG_ID = "BlockEntityTag";
 
     TryReloadBehavior NO_OP = (l, p, i, w) -> {};
 
@@ -31,7 +35,7 @@ public interface TryReloadBehavior {
         var activeAmmunitionId = WeaponItemTagHelper.getOrSetActiveAmmunitionType(itemStack, weaponItemData);
         var ammunitionSupplier = ammunitionStrategy.getAmmunitionSupplierByKeyOrFirst(activeAmmunitionId);
         var ammunition = ammunitionSupplier.get();
-        var ammunitionInInventory = player.getInventory().countItem(ammunition);
+        var ammunitionInInventory = countAmmunitionIncludingShulkerBoxes(player, ammunition);
         var ammunitionInWeapon = WeaponItemTagHelper.getAmmunition(itemStack, weaponItemData);
         var ammunitionMissing = weaponItemData.getAmmunitionStrategy().getMaxAmmunition() - ammunitionInWeapon;
         var ammunitionCountToRestore = AVPPredicates.IS_IMMORTAL.test(player)
@@ -70,22 +74,78 @@ public interface TryReloadBehavior {
             );
     };
 
+    private static int countAmmunitionIncludingShulkerBoxes(ServerPlayer player, Item ammunition) {
+        var rawInventoryItemCount = 0;
+        for (var itemStack : player.getInventory().items) {
+            if (itemStack.is(ammunition)) {
+                rawInventoryItemCount += itemStack.getCount();
+            }
+
+            if (AVPPredicates.IS_ITEM_SHULKER_BOX.test(itemStack.getItem())) {
+                var itemStackTag = itemStack.getTag();
+
+                if (itemStackTag == null) continue;
+
+                var blockEntityTag = itemStackTag.getCompound(BLOCK_ENTITY_TAG_ID);
+                var items = NonNullList.withSize(27, ItemStack.EMPTY);
+                ContainerHelper.loadAllItems(blockEntityTag, items);
+
+                for (var shulkerBoxItemStack: items) {
+                    if (shulkerBoxItemStack.is(ammunition)) {
+                        rawInventoryItemCount += shulkerBoxItemStack.getCount();
+                    }
+                }
+            }
+        }
+
+        return rawInventoryItemCount;
+    }
+
     private static void consumeAmmunitionFromPlayerInventory(ServerPlayer player, int ammunitionCountToRestore, Item ammunition) {
         var counter = ammunitionCountToRestore;
-        for (int i = 0; i < player.getInventory().getContainerSize(); i++) {
+        for (var itemStack : player.getInventory().items) {
             if (counter <= 0) {
                 break;
             }
 
-            var stack = player.getInventory().getItem(i);
-
-            if (Objects.equals(stack.getItem(), ammunition)) {
-                var amountToConsume = Math.min(stack.getCount(), counter);
-                stack.setCount(stack.getCount() - amountToConsume);
+            if (Objects.equals(itemStack.getItem(), ammunition)) {
+                // If the stack item is the same type as the ammunition item, consume part or all of the stack.
+                var count = itemStack.getCount();
+                var amountToConsume = Math.min(count, counter);
+                itemStack.setCount(count - amountToConsume);
                 counter -= amountToConsume;
+            } else if (AVPPredicates.IS_ITEM_SHULKER_BOX.test(itemStack.getItem())) {
+                var itemStackTag = itemStack.getTag();
+
+                if (itemStackTag == null) continue;
+
+                counter = consumeAmmunitionFromShulkerBox(itemStack, itemStackTag, counter);
             }
         }
+
         player.getInventory().setChanged();
+    }
+
+    private static int consumeAmmunitionFromShulkerBox(ItemStack itemStack, CompoundTag itemStackTag, int counter) {
+        var blockEntityTag = itemStackTag.getCompound(BLOCK_ENTITY_TAG_ID);
+        var shulkerBoxItemStacks = NonNullList.withSize(27, ItemStack.EMPTY);
+        ContainerHelper.loadAllItems(blockEntityTag, shulkerBoxItemStacks);
+
+        for (var shulkerBoxItemStack: shulkerBoxItemStacks) {
+            // If the stack item is the same type as the ammunition item, consume part or all of the stack.
+            var count = shulkerBoxItemStack.getCount();
+            var amountToConsume = Math.min(count, counter);
+            shulkerBoxItemStack.setCount(count - amountToConsume);
+            counter -= amountToConsume;
+        }
+
+        // Save the modified shulker box item stacks back to the shulker box block entity tag.
+        ContainerHelper.saveAllItems(blockEntityTag, shulkerBoxItemStacks, false);
+        // Save the block entity tag back to the item stack tag.
+        itemStackTag.put(BLOCK_ENTITY_TAG_ID, blockEntityTag);
+        // Set the item stack tag back on the item stack.
+        itemStack.setTag(itemStackTag);
+        return counter;
     }
 
     void tryReload(ServerLevel serverLevel, ServerPlayer serverPlayer, ItemStack itemStack, WeaponItemData weaponItemData);
--- common/src/main/java/org/avp/common/util/AVPPredicates.java
+++ common/src/main/java/org/avp/common/util/AVPPredicates.java
@@ -1,12 +1,19 @@
 package org.avp.common.util;
 
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 
 import java.util.function.Predicate;
 
 public class AVPPredicates {
 
     public static final Predicate<Player> IS_IMMORTAL = player -> player.isCreative() || player.isSpectator();
+
+    public static final Predicate<Item> IS_ITEM_SHULKER_BOX = item -> item instanceof BlockItem blockItem &&
+        blockItem.getBlock().defaultBlockState().is(BlockTags.SHULKER_BOXES);
 
     private AVPPredicates() {
         throw new UnsupportedOperationException();
