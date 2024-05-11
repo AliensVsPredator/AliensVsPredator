1910c1d62a6ce47f29a1efc5b1d237a8176c730b
bvanseg <bvanseg@gmail.com>
2024-05-11 17:35:10 -0400

    Added missing colored padding block sets.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/common/block/AVPPaddingBlocks.java
+++ common/src/main/java/org/avp/common/block/AVPPaddingBlocks.java
@@ -15,9 +15,37 @@ public class AVPPaddingBlocks extends AVPDeferredBlockRegistry {
 
     public static final AVPPaddingBlocks INSTANCE = new AVPPaddingBlocks();
 
+    public final PaddingBlockSet paddingBlack;
+
+    public final PaddingBlockSet paddingBlue;
+
+    public final PaddingBlockSet paddingBrown;
+
+    public final PaddingBlockSet paddingCyan;
+
+    public final PaddingBlockSet paddingGray;
+
+    public final PaddingBlockSet paddingGreen;
+
+    public final PaddingBlockSet paddingLightBlue;
+
+    public final PaddingBlockSet paddingLightGray;
+
+    public final PaddingBlockSet paddingLime;
+
+    public final PaddingBlockSet paddingMagenta;
+
     public final PaddingBlockSet paddingOrange;
 
+    public final PaddingBlockSet paddingPink;
+
+    public final PaddingBlockSet paddingPurple;
+
+    public final PaddingBlockSet paddingRed;
+
     public final PaddingBlockSet paddingWhite;
+
+    public final PaddingBlockSet paddingYellow;
 
     @Override
     protected Holder<Block> createHolder(String registryName, BlockData.Builder blockDataBuilder) {
@@ -25,8 +53,22 @@ public class AVPPaddingBlocks extends AVPDeferredBlockRegistry {
     }
 
     private AVPPaddingBlocks() {
+        paddingBlack = createSet(Items.BLACK_DYE, "black", BlockBehaviour.Properties.ofFullCopy(Blocks.BLACK_WOOL));
+        paddingBlue = createSet(Items.BLUE_DYE, "blue", BlockBehaviour.Properties.ofFullCopy(Blocks.BLUE_WOOL));
+        paddingBrown = createSet(Items.BROWN_DYE, "brown", BlockBehaviour.Properties.ofFullCopy(Blocks.BROWN_WOOL));
+        paddingCyan = createSet(Items.CYAN_DYE, "cyan", BlockBehaviour.Properties.ofFullCopy(Blocks.CYAN_WOOL));
+        paddingGray = createSet(Items.GRAY_DYE, "gray", BlockBehaviour.Properties.ofFullCopy(Blocks.GRAY_WOOL));
+        paddingGreen = createSet(Items.GREEN_DYE, "green", BlockBehaviour.Properties.ofFullCopy(Blocks.GREEN_WOOL));
+        paddingLightBlue = createSet(Items.LIGHT_BLUE_DYE, "light_blue", BlockBehaviour.Properties.ofFullCopy(Blocks.LIGHT_BLUE_WOOL));
+        paddingLightGray = createSet(Items.LIGHT_GRAY_DYE, "light_gray", BlockBehaviour.Properties.ofFullCopy(Blocks.LIGHT_GRAY_WOOL));
+        paddingLime = createSet(Items.LIME_DYE, "lime", BlockBehaviour.Properties.ofFullCopy(Blocks.LIME_WOOL));
+        paddingMagenta = createSet(Items.MAGENTA_DYE, "magenta", BlockBehaviour.Properties.ofFullCopy(Blocks.MAGENTA_WOOL));
         paddingOrange = createSet(Items.ORANGE_DYE, "orange", BlockBehaviour.Properties.ofFullCopy(Blocks.ORANGE_WOOL));
+        paddingPink = createSet(Items.PINK_DYE, "pink", BlockBehaviour.Properties.ofFullCopy(Blocks.PINK_WOOL));
+        paddingPurple = createSet(Items.PURPLE_DYE, "purple", BlockBehaviour.Properties.ofFullCopy(Blocks.PURPLE_WOOL));
+        paddingRed = createSet(Items.RED_DYE, "red", BlockBehaviour.Properties.ofFullCopy(Blocks.RED_WOOL));
         paddingWhite = createSet(Items.WHITE_DYE, "white", BlockBehaviour.Properties.ofFullCopy(Blocks.WHITE_WOOL));
+        paddingYellow = createSet(Items.YELLOW_DYE, "yellow", BlockBehaviour.Properties.ofFullCopy(Blocks.YELLOW_WOOL));
     }
 
     private PaddingBlockSet createSet(Item dyeItem, String prefix, BlockBehaviour.Properties properties) {
--- common/src/main/java/org/avp/common/data/recipe/impl/block/AVPPaddingBlockRecipes.java
+++ common/src/main/java/org/avp/common/data/recipe/impl/block/AVPPaddingBlockRecipes.java
@@ -11,8 +11,22 @@ import org.avp.common.data.recipe.AVPShapedRecipeBuilder;
 public final class AVPPaddingBlockRecipes {
 
     public static void addPaddingBlockRecipes(RecipeOutput recipeOutput) {
+        addPaddingBlockRecipeSet(AVPPaddingBlocks.INSTANCE.paddingBlack, recipeOutput);
+        addPaddingBlockRecipeSet(AVPPaddingBlocks.INSTANCE.paddingBlue, recipeOutput);
+        addPaddingBlockRecipeSet(AVPPaddingBlocks.INSTANCE.paddingBrown, recipeOutput);
+        addPaddingBlockRecipeSet(AVPPaddingBlocks.INSTANCE.paddingCyan, recipeOutput);
+        addPaddingBlockRecipeSet(AVPPaddingBlocks.INSTANCE.paddingGray, recipeOutput);
+        addPaddingBlockRecipeSet(AVPPaddingBlocks.INSTANCE.paddingGreen, recipeOutput);
+        addPaddingBlockRecipeSet(AVPPaddingBlocks.INSTANCE.paddingLightBlue, recipeOutput);
+        addPaddingBlockRecipeSet(AVPPaddingBlocks.INSTANCE.paddingLightGray, recipeOutput);
+        addPaddingBlockRecipeSet(AVPPaddingBlocks.INSTANCE.paddingLime, recipeOutput);
+        addPaddingBlockRecipeSet(AVPPaddingBlocks.INSTANCE.paddingMagenta, recipeOutput);
         addPaddingBlockRecipeSet(AVPPaddingBlocks.INSTANCE.paddingOrange, recipeOutput);
+        addPaddingBlockRecipeSet(AVPPaddingBlocks.INSTANCE.paddingPink, recipeOutput);
+        addPaddingBlockRecipeSet(AVPPaddingBlocks.INSTANCE.paddingPurple, recipeOutput);
+        addPaddingBlockRecipeSet(AVPPaddingBlocks.INSTANCE.paddingRed, recipeOutput);
         addPaddingBlockRecipeSet(AVPPaddingBlocks.INSTANCE.paddingWhite, recipeOutput);
+        addPaddingBlockRecipeSet(AVPPaddingBlocks.INSTANCE.paddingYellow, recipeOutput);
     }
 
     private static void addPaddingBlockRecipeSet(AVPPaddingBlocks.PaddingBlockSet paddingBlockSet, RecipeOutput recipeOutput) {
