09540c61709ce85b843cb401a5e9baaa48f580f3
bvanseg <bvanseg@gmail.com>
2024-05-16 17:13:23 -0400

    Aliens now only produce acid if damaged by a puncture damage type.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/mixin/MixinLivingEntity_ProduceAcid.java
+++ common/src/main/java/org/avp/mixin/MixinLivingEntity_ProduceAcid.java
@@ -7,6 +7,7 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.level.Level;
+import org.avp.common.tag.AVPDamageTypeTags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -29,6 +30,10 @@ public abstract class MixinLivingEntity_ProduceAcid extends Entity {
         var level = self.level();
 
         if (!level.isClientSide() && self.getType().is(AVPEntityTags.ACID_BLEEDERS)) {
+            if (!damageSource.is(AVPDamageTypeTags.PUNCTURE)) {
+                return;
+            }
+
             var box = self.getBoundingBox();
             var pos = self.blockPosition();
             var randomX = random.nextInt((int) Math.ceil(box.maxX - box.minX)) * (random.nextBoolean() ? -1 : 1);
