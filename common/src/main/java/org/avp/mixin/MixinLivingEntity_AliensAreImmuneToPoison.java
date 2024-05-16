acdd4e5e13c2d0a43afdef58cfac23b8cdf4c57e
bvanseg <bvanseg@gmail.com>
2024-05-16 16:32:56 -0400

    Aliens are now immune to poison.
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ common/src/main/java/org/avp/mixin/MixinLivingEntity_AliensAreImmuneToPoison.java
@@ -0,0 +1,30 @@
+package org.avp.mixin;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import org.avp.common.tag.AVPEntityTags;
+import org.avp.common.util.MixinUtils;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class MixinLivingEntity_AliensAreImmuneToPoison extends Entity {
+
+    protected MixinLivingEntity_AliensAreImmuneToPoison(EntityType<?> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Inject(at = @At("HEAD"), cancellable = true, method = "canBeAffected")
+    void ignorePoisonDamage(MobEffectInstance mobEffectInstance, CallbackInfoReturnable<Boolean> callbackInfoReturnable) {
+        var self = MixinUtils.<LivingEntity>self(this);
+        if (self.getType().is(AVPEntityTags.ALIENS) && mobEffectInstance.getEffect() == MobEffects.POISON) {
+            callbackInfoReturnable.setReturnValue(false);
+        }
+    }
+}
