dc41cfd4f82f0589cc7a2c40f76a4c88bfab76d3
bvanseg <bvanseg@gmail.com>
2024-05-16 16:22:07 -0400

    Aliens no longer suffocate in walls.
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ common/src/main/java/org/avp/mixin/MixinLivingEntityAliensDoNotSuffocate.java
@@ -0,0 +1,30 @@
+package org.avp.mixin;
+
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import org.avp.common.tag.AVPEntityTags;
+import org.avp.common.util.MixinUtils;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class MixinLivingEntityAliensDoNotSuffocate extends Entity {
+
+    protected MixinLivingEntityAliensDoNotSuffocate(EntityType<?> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Inject(at = @At("HEAD"), cancellable = true, method = "hurt")
+    void ignoreSuffocationDamage(DamageSource damageSource, float damage, CallbackInfoReturnable<Boolean> callbackInfoReturnable) {
+        var self = MixinUtils.<LivingEntity>self(this);
+        if (self.getType().is(AVPEntityTags.ALIENS) && damageSource.is(DamageTypes.IN_WALL)) {
+            callbackInfoReturnable.setReturnValue(false);
+        }
+    }
+}
