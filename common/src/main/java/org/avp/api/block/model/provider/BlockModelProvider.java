c0e71c6d1bdff31db83fbfbcd45d216adaf6f280
bvanseg <bvanseg@gmail.com>
2024-05-24 05:24:34 -0400

    Removed dead code.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/api/block/model/provider/BlockModelProvider.java
+++ common/src/main/java/org/avp/api/block/model/provider/BlockModelProvider.java
@@ -1,12 +1,7 @@
 package org.avp.api.block.model.provider;
 
 import net.minecraft.data.models.BlockModelGenerators;
-import org.avp.api.block.model.render_type.BlockModelRenderType;
 
 public abstract class BlockModelProvider {
     public abstract void run(BlockModelGenerators blockModelGenerators);
-
-    public BlockModelRenderType getRenderType() {
-        return BlockModelRenderType.NORMAL;
-    }
 }
