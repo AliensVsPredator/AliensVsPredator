b2b4bb6b0abd30786390f016de07d8f961868891
bvanseg <bvanseg@gmail.com>
2024-05-26 06:38:39 -0400

    Added entity gun immunity tags.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/common/tag/AVPEntityTypeTags.java
+++ common/src/main/java/org/avp/common/tag/AVPEntityTypeTags.java
@@ -18,7 +18,11 @@ public class AVPEntityTypeTags {
 
     public static final TagKey<EntityType<?>> ENGINEERS = create("engineers");
 
+    public static final TagKey<EntityType<?>> HEAVY_GUNS_IMMUNE = create("heavy_guns_immune");
+
     public static final TagKey<EntityType<?>> HIVE_ALIENS = create("hive_aliens");
+
+    public static final TagKey<EntityType<?>> MEDIUM_GUNS_IMMUNE = create("medium_guns_immune");
 
     public static final TagKey<EntityType<?>> MONSTERS = create("monsters");
 
@@ -33,6 +37,10 @@ public class AVPEntityTypeTags {
     public static final TagKey<EntityType<?>> PRODUCES_RESIN = create("produces_resin");
 
     public static final TagKey<EntityType<?>> ROYAL_ALIENS = create("royal_aliens");
+
+    public static final TagKey<EntityType<?>> SMALL_GUNS_IMMUNE = create("small_guns_immune");
+
+    public static final TagKey<EntityType<?>> UBER_GUNS_IMMUNE = create("uber_guns_immune");
 
     private static TagKey<EntityType<?>> create(String registryName) {
         return TagKey.create(Registries.ENTITY_TYPE, AVPResources.location(registryName));
