09b6bed1fca4b7c0ce10a0de1fbfcca99bae99b7
bvanseg <bvanseg@gmail.com>
2024-05-26 06:47:37 -0400

    Added a slight tremble to boiler.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/client/render/entity/living/BoilerRenderer.java
+++ common/src/main/java/org/avp/client/render/entity/living/BoilerRenderer.java
@@ -3,13 +3,21 @@ package org.avp.client.render.entity.living;
 import mod.azure.azurelib.common.api.client.renderer.GeoEntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 
+import net.minecraft.world.phys.Vec3;
 import org.avp.client.model.entity.living.BoilerModel;
 import org.avp.common.entity.living.Boiler;
+import org.jetbrains.annotations.NotNull;
 
 public class BoilerRenderer extends GeoEntityRenderer<Boiler> {
 
     public BoilerRenderer(EntityRendererProvider.Context context) {
         super(context, new BoilerModel());
         this.withScale(0.9F);
+    }
+
+    @Override
+    public @NotNull Vec3 getRenderOffset(Boiler boiler, float partialTicks) {
+        var shakeIntensity = 0.0025;
+        return new Vec3(boiler.getRandom().nextGaussian() * shakeIntensity, 0.0, boiler.getRandom().nextGaussian() * shakeIntensity);
     }
 }
