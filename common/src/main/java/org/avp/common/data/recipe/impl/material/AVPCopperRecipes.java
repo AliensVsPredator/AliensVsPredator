359afda68ba98c2edf59c68e80a37198c3c72e16
bvanseg <bvanseg@gmail.com>
2024-05-27 06:00:15 -0400

    Temp fix for build error.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/common/data/recipe/impl/material/AVPCopperRecipes.java
+++ common/src/main/java/org/avp/common/data/recipe/impl/material/AVPCopperRecipes.java
@@ -2,13 +2,16 @@ package org.avp.common.data.recipe.impl.material;
 
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.world.level.block.Blocks;
+import org.avp.api.Holder;
 import org.avp.common.block.AVPBlocks;
 import org.avp.common.data.recipe.AVPMetalRecipeHelper;
 
 public final class AVPCopperRecipes {
 
     public static void addCopperRecipes(RecipeOutput recipeOutput) {
-        AVPMetalRecipeHelper.addMetalSetRecipes(recipeOutput, Blocks.COPPER_BLOCK, AVPBlocks.INSTANCE.copper);
+        // TODO: This is incredibly hacky, need to fix.
+        var copperHolder = new Holder<>("null", () -> Blocks.COPPER_BLOCK);
+        AVPMetalRecipeHelper.addMetalSetRecipes(recipeOutput, copperHolder, AVPBlocks.INSTANCE.copper);
     }
 
     private AVPCopperRecipes() {
