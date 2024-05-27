f83684d13d0e0b41785d571fae872253ef52d113
bvanseg <bvanseg@gmail.com>
2024-05-27 04:21:56 -0400

    Added ammo type indicator rendering for weapons in item slots.
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ common/src/main/java/org/avp/mixin/client/MixinGuiGraphics.java
@@ -0,0 +1,60 @@
+package org.avp.mixin.client;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.avp.api.item.weapon.WeaponItemTagHelper;
+import org.avp.common.item.AbstractAVPWeaponItem;
+import org.avp.common.util.MixinUtils;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import javax.annotation.Nullable;
+
+@Mixin(GuiGraphics.class)
+public abstract class MixinGuiGraphics {
+
+    @Inject(
+        at = @At(
+            value = "HEAD",
+            target = "net/minecraft/client/gui/GuiGraphics.renderItem(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;IIII)V"
+        ),
+        method = "net/minecraft/client/gui/GuiGraphics.renderItem(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;IIII)V",
+        cancellable = true
+    )
+    public void renderItem(
+        @Nullable LivingEntity livingEntity,
+        @Nullable Level level,
+        ItemStack itemStack,
+        int x,
+        int y,
+        int $0,
+        int $1,
+        CallbackInfo callbackInfo
+    ) {
+        if (itemStack.isEmpty())
+            return;
+
+        var item = itemStack.getItem();
+
+        if (!(item instanceof AbstractAVPWeaponItem weaponItem))
+            return;
+
+        if (!itemStack.hasTag())
+            return;
+
+        var self = MixinUtils.<GuiGraphics>self(this);
+        var weaponData = weaponItem.getWeaponItemData();
+        var bulletEffects = WeaponItemTagHelper.getBulletEffects(itemStack, weaponData);
+
+        if (bulletEffects == null)
+            return;
+
+        bulletEffects.stream()
+            .findFirst()
+            .ifPresent(bulletEffect -> self.renderOutline(x, y, 16, 16, bulletEffect.getColor()));
+    }
+}
