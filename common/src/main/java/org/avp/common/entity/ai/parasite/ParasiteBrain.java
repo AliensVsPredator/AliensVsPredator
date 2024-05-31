1253fb5a0b15f622ce48e84265b53a8ee2a2ce14
bvanseg <bvanseg@gmail.com>
2024-05-31 09:48:18 -0400

    ParasiteBrain.java refactor.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/common/entity/ai/parasite/ParasiteBrain.java
+++ common/src/main/java/org/avp/common/entity/ai/parasite/ParasiteBrain.java
@@ -3,12 +3,12 @@ package org.avp.common.entity.ai.parasite;
 import net.minecraft.world.entity.monster.Monster;
 import org.avp.api.entity.Parasite;
 import org.avp.api.entity.ai.GOAPBrain;
-import org.avp.api.entity.ai.goal.impl.IdleMoveGoal;
 import org.avp.api.entity.ai.plan.Planner;
 import org.avp.api.entity.ai.sensor.impl.NearbyEntitiesSensor;
 import org.avp.api.entity.ai.sensor.impl.NearbyLivingEntitiesSensor;
 import org.avp.api.entity.ai.sensor.impl.TargetSensor;
 import org.avp.common.entity.ai.parasite.goal.fertile.AttachToHostGoal;
+import org.avp.common.entity.ai.parasite.goal.fertile.ParasiteIdleMoveGoal;
 import org.avp.common.entity.ai.parasite.goal.infertile.DetachFromHostGoal;
 import org.avp.common.entity.ai.parasite.goal.fertile.ImpregnateHostGoal;
 import org.avp.common.entity.ai.parasite.goal.fertile.MoveToHostGoal;
@@ -18,11 +18,13 @@ import java.util.Set;
 
 public class ParasiteBrain extends GOAPBrain {
 
+    private static final int PARASITE_SMOTHER_TIME = 5 * 60 * 20;
+
     public ParasiteBrain(Monster parasite) {
         super(
             new Planner(
                 Set.of(
-                    new IdleMoveGoal(parasite),
+                    new ParasiteIdleMoveGoal(parasite),
 
                     // Anticipated plan for impregnation
                     new MoveToHostGoal(parasite),
@@ -30,7 +32,7 @@ public class ParasiteBrain extends GOAPBrain {
                     new ImpregnateHostGoal(parasite),
 
                     // Anticipated plan for smothering
-                    new DetachFromHostGoal(parasite, 5 * 60 * 20)
+                    new DetachFromHostGoal(parasite, PARASITE_SMOTHER_TIME)
                 )
             )
         );
