fb25419d0b499b2e1b74f5b9d688551e157bb8d5
bvanseg <bvanseg@gmail.com>
2024-05-31 10:14:19 -0400

    Added parasite behavior to royal facehugger.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/common/entity/living/FacehuggerRoyal.java
+++ common/src/main/java/org/avp/common/entity/living/FacehuggerRoyal.java
@@ -1,43 +1,20 @@
 package org.avp.common.entity.living;
 
-import mod.azure.azurelib.common.api.common.animatable.GeoEntity;
-import mod.azure.azurelib.common.internal.common.core.animatable.instance.AnimatableInstanceCache;
 import mod.azure.azurelib.common.internal.common.core.animation.AnimatableManager;
-import mod.azure.azurelib.common.internal.common.util.AzureLibUtil;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
-
 import org.avp.common.animation.FacehuggerRoyalAnimations;
-import org.avp.common.entity.ai.AIUtils;
+import org.avp.common.entity.AVPAbstractParasite;
 
-public class FacehuggerRoyal extends Monster implements GeoEntity {
-
-    private final AnimatableInstanceCache cache = AzureLibUtil.createInstanceCache(this);
+public class FacehuggerRoyal extends AVPAbstractParasite {
 
     public FacehuggerRoyal(EntityType<? extends Monster> entityType, Level level) {
         super(entityType, level);
     }
 
     @Override
-    protected void registerGoals() {
-        AIUtils.addFacehuggerAI(this, goalSelector, targetSelector);
-    }
-
-    @Override
-    public boolean doHurtTarget(@NotNull Entity entity) {
-        return true;
-    }
-
-    @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
         FacehuggerRoyalAnimations.bootstrap(this, controllers);
-    }
-
-    @Override
-    public AnimatableInstanceCache getAnimatableInstanceCache() {
-        return cache;
     }
 }
