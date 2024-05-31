f97587a04641a5130ecfb56bf5507135d2746805
bvanseg <bvanseg@gmail.com>
2024-05-31 10:14:33 -0400

    Added parasite behavior to octohugger.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/common/entity/living/Octohugger.java
+++ common/src/main/java/org/avp/common/entity/living/Octohugger.java
@@ -1,16 +1,12 @@
 package org.avp.common.entity.living;
 
-import mod.azure.azurelib.common.api.common.animatable.GeoEntity;
-import mod.azure.azurelib.common.internal.common.core.animatable.instance.AnimatableInstanceCache;
 import mod.azure.azurelib.common.internal.common.core.animation.AnimatableManager;
-import mod.azure.azurelib.common.internal.common.util.AzureLibUtil;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
+import org.avp.common.entity.AVPAbstractParasite;
 
-public class Octohugger extends Monster implements GeoEntity {
-
-    private final AnimatableInstanceCache cache = AzureLibUtil.createInstanceCache(this);
+public class Octohugger extends AVPAbstractParasite {
 
     public Octohugger(EntityType<? extends Monster> entityType, Level level) {
         super(entityType, level);
@@ -19,10 +15,5 @@ public class Octohugger extends Monster implements GeoEntity {
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
         // TODO:
-    }
-
-    @Override
-    public AnimatableInstanceCache getAnimatableInstanceCache() {
-        return cache;
     }
 }
