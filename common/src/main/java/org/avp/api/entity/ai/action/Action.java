5970c407ef3a5a75e7743da4420bf35993488625
bvanseg <bvanseg@gmail.com>
2024-06-01 04:58:26 -0400

    Fixed mobs still having AI while parasite is attached.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/api/entity/ai/action/Action.java
+++ common/src/main/java/org/avp/api/entity/ai/action/Action.java
@@ -1,6 +1,9 @@
 package org.avp.api.entity.ai.action;
 
 public abstract class Action {
+    /**
+     * The cost to execute this action. Lower is better.
+     */
     public abstract int getCost();
     public abstract void execute();
 }
--- common/src/main/java/org/avp/common/entity/ai/parasite/action/infertile/DetachFromHostAction.java
+++ common/src/main/java/org/avp/common/entity/ai/parasite/action/infertile/DetachFromHostAction.java
@@ -1,7 +1,9 @@
 package org.avp.common.entity.ai.parasite.action.infertile;
 
+import net.minecraft.world.entity.Mob;
 import org.avp.api.entity.ai.action.Action;
 import org.avp.common.entity.AVPAbstractParasite;
+import org.avp.common.util.GravityUtils;
 
 public class DetachFromHostAction extends Action {
 
@@ -22,8 +24,16 @@ public class DetachFromHostAction extends Action {
     @Override
     public void execute() {
         if (parasite.getTicksAttachedToHost() >= smotherTimeInTicks) {
+            if (parasite.getVehicle() instanceof Mob mob) {
+                mob.setNoAi(false);
+            }
             parasite.stopRiding();
         } else {
+            // TODO: Move this to a cleanup function, otherwise the mob can be left in a broken state.
+            if (parasite.getVehicle() instanceof Mob mob) {
+                mob.setNoAi(true);
+                GravityUtils.apply(mob);
+            }
             parasite.incrementTicksAttachedToHost();
         }
     }
