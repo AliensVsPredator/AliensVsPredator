dea5e02573767e4c71afc92e96a67aa45381fd5e
bvanseg <bvanseg@gmail.com>
2024-06-01 04:51:25 -0400

    Added GravityUtils.java.
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ common/src/main/java/org/avp/common/util/GravityUtils.java
@@ -0,0 +1,17 @@
+package org.avp.common.util;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.MoverType;
+
+public class GravityUtils {
+
+    public static void apply(Entity entity) {
+        entity.setDeltaMovement(0, entity.getDeltaMovement().y - 0.03999999910593033D, 0);
+        entity.move(MoverType.SELF, entity.getDeltaMovement());
+        entity.setDeltaMovement(0, entity.getDeltaMovement().y * 0.9800000190734863D, 0);
+    }
+
+    private GravityUtils() {
+        throw new UnsupportedOperationException();
+    }
+}
