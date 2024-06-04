23d7f476d12074a762581c6618b16bae4129707c
bvanseg <bvanseg@gmail.com>
2024-06-04 04:37:15 -0400

    Fixed hosts not being restored on parasite despawn.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/common/entity/AVPAbstractParasite.java
+++ common/src/main/java/org/avp/common/entity/AVPAbstractParasite.java
@@ -9,6 +9,7 @@ import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
 import org.avp.api.entity.GOAPBrainUser;
@@ -17,6 +18,7 @@ import org.avp.api.entity.ai.GOAPBrain;
 import org.avp.api.entity.data.sync.SyncedDataHandle;
 import org.avp.api.entity.data.sync.SyncedDataSerializer;
 import org.avp.common.entity.ai.parasite.ParasiteBrain;
+import org.avp.common.util.GravityUtils;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class AVPAbstractParasite extends Monster implements GeoEntity, GOAPBrainUser, Parasite {
@@ -46,6 +48,13 @@ public abstract class AVPAbstractParasite extends Monster implements GeoEntity, 
     public void readAdditionalSaveData(@NotNull CompoundTag compoundTag) {
         super.readAdditionalSaveData(compoundTag);
         ticksAttachedToHost = compoundTag.getInt(TICKS_ATTACHED_TO_HOST_KEY);
+    }
+
+    @Override
+    public void remove(@NotNull RemovalReason removalReason) {
+        // We don't really care why the parasite is removed, we just need to restore the host's state.
+        restoreHost();
+        super.remove(removalReason);
     }
 
     @Override
@@ -81,5 +90,34 @@ public abstract class AVPAbstractParasite extends Monster implements GeoEntity, 
     @Override
     public GOAPBrain createGOAPBrain() {
         return new ParasiteBrain(this);
+    }
+
+    public void smotherHost() {
+        var vehicle = getVehicle();
+
+        if (vehicle == null) return;
+
+        if (vehicle instanceof Mob mob) {
+            mob.setNoAi(true);
+        }
+
+        // Apply gravity while mob's "AI" is off.
+        GravityUtils.apply(vehicle);
+        // Facehuggers provide oxygen to their host.
+        vehicle.setAirSupply(vehicle.getMaxAirSupply());
+        // Parasites silence host.
+        vehicle.setSilent(true);
+    }
+
+    public void restoreHost() {
+        var vehicle = getVehicle();
+
+        if (vehicle == null) return;
+
+        // Reset state.
+        if (vehicle instanceof Mob mob) {
+            mob.setNoAi(false);
+        }
+        vehicle.setSilent(false);
     }
 }
--- common/src/main/java/org/avp/common/entity/ai/parasite/action/SmotherHostAction.java
+++ common/src/main/java/org/avp/common/entity/ai/parasite/action/SmotherHostAction.java
@@ -1,12 +1,8 @@
 package org.avp.common.entity.ai.parasite.action;
 
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Mob;
 import org.avp.api.entity.ai.action.Action;
 import org.avp.common.entity.AVPAbstractParasite;
-import org.avp.common.util.GravityUtils;
-
-import java.util.Objects;
 
 public class SmotherHostAction extends Action {
 
@@ -31,28 +27,13 @@ public class SmotherHostAction extends Action {
 
     @Override
     public void execute() {
-        var vehicle = (LivingEntity) Objects.requireNonNull(parasite.getVehicle());
-
-        // TODO: Move this to a cleanup function, otherwise the mob can be left in a broken state.
-        if (parasite.getVehicle() instanceof Mob mob) {
-            mob.setNoAi(true);
-        }
-
         // Tick counter.
         parasite.incrementTicksAttachedToHost();
-        // Apply gravity while mob's "AI" is off.
-        GravityUtils.apply(vehicle);
-        // Facehuggers provide oxygen to their host.
-        vehicle.setAirSupply(vehicle.getMaxAirSupply());
-        // Parasites silence host.
-        vehicle.setSilent(true);
+        parasite.smotherHost();
 
         if (parasite.getTicksAttachedToHost() >= smotherTimeInTicks) {
             // Reset state.
-            if (vehicle instanceof Mob mob) {
-                mob.setNoAi(false);
-            }
-            vehicle.setSilent(false);
+            parasite.restoreHost();
             parasite.stopRiding();
         }
     }
