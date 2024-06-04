beda6c5f77a53af91befb2b5faabd71adde8edbd
bvanseg <bvanseg@gmail.com>
2024-06-04 04:17:58 -0400

    Silence host, provide host air while parasite smothers.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/common/entity/ai/parasite/action/DetachFromHostAction.java
+++ common/src/main/java/org/avp/common/entity/ai/parasite/action/DetachFromHostAction.java
@@ -1,9 +1,12 @@
 package org.avp.common.entity.ai.parasite.action;
 
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import org.avp.api.entity.ai.action.Action;
 import org.avp.common.entity.AVPAbstractParasite;
 import org.avp.common.util.GravityUtils;
+
+import java.util.Objects;
 
 public class DetachFromHostAction extends Action {
 
@@ -18,7 +21,7 @@ public class DetachFromHostAction extends Action {
 
     @Override
     public boolean isValid() {
-        return parasite.getVehicle() instanceof Mob;
+        return parasite.getVehicle() instanceof LivingEntity;
     }
 
     @Override
@@ -28,18 +31,29 @@ public class DetachFromHostAction extends Action {
 
     @Override
     public void execute() {
+        var vehicle = (LivingEntity) Objects.requireNonNull(parasite.getVehicle());
+
+        // TODO: Move this to a cleanup function, otherwise the mob can be left in a broken state.
+        if (parasite.getVehicle() instanceof Mob mob) {
+            mob.setNoAi(true);
+        }
+
+        // Tick counter.
+        parasite.incrementTicksAttachedToHost();
+        // Apply gravity while mob's "AI" is off.
+        GravityUtils.apply(vehicle);
+        // Facehuggers provide oxygen to their host.
+        vehicle.setAirSupply(vehicle.getMaxAirSupply());
+        // Parasites silence host.
+        vehicle.setSilent(true);
+
         if (parasite.getTicksAttachedToHost() >= smotherTimeInTicks) {
-            if (parasite.getVehicle() instanceof Mob mob) {
+            // Reset state.
+            if (vehicle instanceof Mob mob) {
                 mob.setNoAi(false);
             }
+            vehicle.setSilent(false);
             parasite.stopRiding();
-        } else {
-            // TODO: Move this to a cleanup function, otherwise the mob can be left in a broken state.
-            if (parasite.getVehicle() instanceof Mob mob) {
-                mob.setNoAi(true);
-                GravityUtils.apply(mob);
-            }
-            parasite.incrementTicksAttachedToHost();
         }
     }
 }
