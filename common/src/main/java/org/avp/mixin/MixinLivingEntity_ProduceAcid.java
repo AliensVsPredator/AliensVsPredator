1da485f3deb613f55b28f73d5f2453d0322c4663
bvanseg <bvanseg@gmail.com>
2024-06-07 01:18:30 -0400

    Acid now spawns with varying strength (depends on damage dealt to xenomorph).
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/mixin/MixinLivingEntity_ProduceAcid.java
+++ common/src/main/java/org/avp/mixin/MixinLivingEntity_ProduceAcid.java
@@ -1,14 +1,14 @@
 package org.avp.mixin;
 
-import net.minecraft.server.level.ServerLevel;
+import net.minecraft.core.BlockPos;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -33,20 +33,43 @@ public abstract class MixinLivingEntity_ProduceAcid extends Entity {
         var level = self.level();
 
         if (!level.isClientSide() && self.getType().is(AVPEntityTypeTags.ACID_BLEEDERS)) {
-            if (
-                damage > self.getMaxHealth() * 0.1 ||
-                    self.getHealth() <= self.getMaxHealth() * 0.33 ||
-                    damageSource.is(AVPDamageTypeTags.IS_PUNCTURING) ||
-                    hurtByPlayerWithAxeOrSwordWeapon(damageSource)
-            ) {
-                var box = self.getBoundingBox();
-                var pos = self.blockPosition();
-                var randomX = random.nextInt((int) Math.ceil(box.maxX - box.minX)) * (random.nextBoolean() ? -1 : 1);
-                var randomZ = random.nextInt((int) Math.ceil(box.maxZ - box.minZ)) * (random.nextBoolean() ? -1 : 1);
-                var randomPos = pos.offset(randomX, 0, randomZ);
-                AcidData.INSTANCE.getHolder().get().spawn((ServerLevel) level, randomPos, MobSpawnType.NATURAL);
+            var tenPercentMaxHealth = self.getMaxHealth() * 0.1;
+            if (doesAttackHurtXenomorphBadly(damageSource, damage, tenPercentMaxHealth, self)) {
+                var randomPos = computeRandomPosFromBoundingBox(self);
+                spawnAcid(damage, level, randomPos, self, tenPercentMaxHealth);
             }
         }
+    }
+
+    @Unique
+    private void spawnAcid(float damage, Level level, BlockPos randomPos, LivingEntity self, double tenPercentMaxHealth) {
+        var acidEntityType = AcidData.INSTANCE.getHolder().get();
+        var acidEntity = acidEntityType.create(level);
+
+        if (acidEntity != null) {
+            acidEntity.moveTo(randomPos, self.getYRot(), self.getXRot());
+            level.addFreshEntity(acidEntity);
+            // Acid multiplier scales with how much damage was dealt in this attack.
+            var multiplier = Math.max((int) (damage / tenPercentMaxHealth), 1);
+            acidEntity.setMultiplier(multiplier);
+        }
+    }
+
+    @Unique
+    private boolean doesAttackHurtXenomorphBadly(DamageSource damageSource, float damage, double tenPercentMaxHealth, LivingEntity self) {
+        return damage > tenPercentMaxHealth ||
+            self.getHealth() <= self.getMaxHealth() * 0.33 ||
+            damageSource.is(AVPDamageTypeTags.IS_PUNCTURING) ||
+            hurtByPlayerWithAxeOrSwordWeapon(damageSource);
+    }
+
+    @Unique
+    private @NotNull BlockPos computeRandomPosFromBoundingBox(LivingEntity self) {
+        var box = self.getBoundingBox();
+        var pos = self.blockPosition();
+        var randomX = random.nextInt((int) Math.ceil(box.maxX - box.minX)) * (random.nextBoolean() ? -1 : 1);
+        var randomZ = random.nextInt((int) Math.ceil(box.maxZ - box.minZ)) * (random.nextBoolean() ? -1 : 1);
+        return pos.offset(randomX, 0, randomZ);
     }
 
     @Unique
