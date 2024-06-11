0797d4f856b1e61210a7577646456faffdd8180b
bvanseg <bvanseg@gmail.com>
2024-06-11 19:50:06 -0400

    Stained glass can now be blasted into industrial glass.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/common/data/recipe/impl/block/AVPIndustrialGlassBlockRecipes.java
+++ common/src/main/java/org/avp/common/data/recipe/impl/block/AVPIndustrialGlassBlockRecipes.java
@@ -7,12 +7,36 @@ import net.minecraft.world.item.DyeItem;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import org.avp.common.AVPConstants;
+import org.avp.common.data.recipe.AVPRecipeHelper;
 import org.avp.common.registry.block.AVPIndustrialGlassBlockRegistry;
 import org.avp.common.data.recipe.AVPRecipeProvider;
 import org.avp.common.data.recipe.AVPShapedRecipeBuilder;
 
+import java.util.Set;
+
 public final class AVPIndustrialGlassBlockRecipes {
+
+    private static final Set<Block> STAINED_GLASS_BLOCKS = Set.of(
+        Blocks.BLACK_STAINED_GLASS,
+        Blocks.BLUE_STAINED_GLASS,
+        Blocks.BROWN_STAINED_GLASS,
+        Blocks.CYAN_STAINED_GLASS,
+        Blocks.GRAY_STAINED_GLASS,
+        Blocks.GREEN_STAINED_GLASS,
+        Blocks.LIGHT_BLUE_STAINED_GLASS,
+        Blocks.LIGHT_GRAY_STAINED_GLASS,
+        Blocks.LIME_STAINED_GLASS,
+        Blocks.MAGENTA_STAINED_GLASS,
+        Blocks.ORANGE_STAINED_GLASS,
+        Blocks.PINK_STAINED_GLASS,
+        Blocks.PURPLE_STAINED_GLASS,
+        Blocks.RED_STAINED_GLASS,
+        Blocks.WHITE_STAINED_GLASS,
+        Blocks.YELLOW_STAINED_GLASS
+    );
 
     public static void addIndustrialGlassBlockRecipes(RecipeOutput recipeOutput) {
         // Industrial glass originates from blasting regular glass.
@@ -46,6 +70,19 @@ public final class AVPIndustrialGlassBlockRecipes {
                 .unlockedBy("has_industrial_glass", AVPRecipeProvider.has(industrialGlassOutput))
                 .save(recipeOutput, AVPConstants.MOD_ID + ":industrial_glass_from_blasting_" + coloredIndustrialGlassName);
         });
+
+        STAINED_GLASS_BLOCKS.forEach(stainedGlassBlock ->
+            // All stained glass blocks can be blasted again to remove dyed colors.
+            SimpleCookingRecipeBuilder.blasting(
+                    Ingredient.of(stainedGlassBlock),
+                    RecipeCategory.MISC,
+                    industrialGlassOutput,
+                    0,
+                    100
+                )
+                .unlockedBy("has_industrial_glass", AVPRecipeProvider.has(industrialGlassOutput))
+                .save(recipeOutput, AVPConstants.MOD_ID + ":industrial_glass_from_blasting_" + AVPRecipeHelper.getItemName(stainedGlassBlock))
+        );
     }
 
     private AVPIndustrialGlassBlockRecipes() {
