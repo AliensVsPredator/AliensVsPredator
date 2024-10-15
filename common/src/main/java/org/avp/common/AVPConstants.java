acc5fdb3633be81c531fb37a5f0bbd267a5fab7e
AzureDoom <jerry99666@gmail.com>
2024-10-15 05:25:34 -0400

    Move to BLPredicates instead

--- common/src/main/java/org/avp/common/AVPConstants.java
+++ common/src/main/java/org/avp/common/AVPConstants.java
@@ -1,7 +1,5 @@
 package org.avp.common;
 
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,8 +10,6 @@ public class AVPConstants {
     public static final String MOD_NAME = "AliensVsPredator";
 
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
-
-    public static Predicate<Entity> isCreativeSpecPlayer = entity -> (entity instanceof Player playerEntity && (playerEntity.isCreative() || playerEntity.isSpectator()));
 
     private AVPConstants() {
         throw new UnsupportedOperationException();
