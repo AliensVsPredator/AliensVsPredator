69544fc9f5d2adab0a156ce529f06acae04e6ede
AzureDoom <jerry99666@gmail.com>
2024-10-18 11:15:41 -0400

    Fixes building due to missing import

--- common/src/main/java/org/avp/common/ai/AIUtils.java
+++ common/src/main/java/org/avp/common/ai/AIUtils.java
@@ -1,6 +1,7 @@
 package org.avp.common.ai;
 
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.AvoidEntityGoal;
 import net.minecraft.world.entity.ai.goal.FloatGoal;
 import net.minecraft.world.entity.ai.goal.GoalSelector;
