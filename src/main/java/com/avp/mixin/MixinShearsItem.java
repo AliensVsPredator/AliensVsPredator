be7654228b40222b082fd1a77f275017af3b0e7a
GityBoyy <adarsh43122@gmail.com>
2025-03-05 18:21:58 +0530

    Shear Fix

---
+++ src/main/java/com/avp/mixin/MixinShearsItem.java
@@ -0,0 +1,40 @@
+package com.avp.mixin;
+
+import com.avp.common.block.AVPBlocks;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ShearsItem;
+import net.minecraft.world.item.component.Tool;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mixin(ShearsItem.class)
+public class MixinShearsItem {
+    @Inject(method = "createToolProperties", at = @At("RETURN"), cancellable = true)
+    private static void avp$createToolProperties(CallbackInfoReturnable<Tool> cir) {
+        Tool originalTool = cir.getReturnValue();
+
+        if (originalTool != null) {
+            List<Tool.Rule> modifiedRules = new ArrayList<>(originalTool.rules());
+            modifiedRules.add(Tool.Rule.minesAndDrops(List.of(AVPBlocks.RAZOR_WIRE), 15.0F));
+            Tool modifiedTool = new Tool(modifiedRules, originalTool.defaultMiningSpeed(), 1);
+
+            cir.setReturnValue(modifiedTool);
+        }
+    }
+
+    @Inject(method = "mineBlock", at = @At("RETURN"), cancellable = true)
+    private void avp$mineBlock(ItemStack itemStack, Level level, BlockState blockState, BlockPos blockPos, LivingEntity livingEntity, CallbackInfoReturnable<Boolean> cir)
+    {
+        cir.setReturnValue(blockState.is(AVPBlocks.RAZOR_WIRE));
+    }
+}
