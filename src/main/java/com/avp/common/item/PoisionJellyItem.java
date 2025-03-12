42d8f6c6544b61d699d49a0159eac2859a86c772
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-03-12 14:37:30 -0400

    Update PoisionJellyItem.java

--- src/main/java/com/avp/common/item/PoisionJellyItem.java
+++ src/main/java/com/avp/common/item/PoisionJellyItem.java
@@ -1,12 +1,13 @@
 package com.avp.common.item;
 
-import com.avp.common.entity.living.alien.xenomorph.Xenomorph;
+import com.avp.common.entity.living.alien.Alien;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 public class PoisionJellyItem extends Item {
 
@@ -15,9 +16,9 @@ public class PoisionJellyItem extends Item {
     }
 
     @Override
-    public InteractionResult interactLivingEntity(ItemStack itemStack, Player player, LivingEntity livingEntity, InteractionHand interactionHand) {
-        if (livingEntity instanceof Xenomorph xenomorph) {
-            xenomorph.getEntityData().set(Xenomorph.IS_POISONED, true);
+    public @NotNull InteractionResult interactLivingEntity(ItemStack itemStack, Player player, LivingEntity livingEntity, InteractionHand interactionHand) {
+        if (livingEntity instanceof Alien xenomorph) {
+            xenomorph.getEntityData().set(Alien.IS_POISONED, true);
         }
         return super.interactLivingEntity(itemStack, player, livingEntity, interactionHand);
     }
