8ba8ac7040c8f7d3b39bfe0f9b1b8090019b6dab
GityBoyy <adarsh43122@gmail.com>
2025-03-13 22:16:13 +0530

    fixes

--- src/main/java/com/avp/data/recipe/impl/MetalRecipeProvider.java
+++ src/main/java/com/avp/data/recipe/impl/MetalRecipeProvider.java
@@ -18,38 +18,38 @@ public class MetalRecipeProvider {
 
     public static void provide(RecipeBuilder builder) {
         builder.shapeless()
-            .withCategory(RecipeCategory.MISC)
-            .requires(1, AVPItems.CARBON_DUST)
-            .requires(1, Items.RAW_IRON)
-            .into(1, AVPItems.RAW_CRUDE_IRON);
+                .withCategory(RecipeCategory.MISC)
+                .requires(1, AVPItems.CARBON_DUST)
+                .requires(1, Items.RAW_IRON)
+                .into(1, AVPItems.RAW_CRUDE_IRON);
 
         builder.shapeless()
-            .withCategory(RecipeCategory.MISC)
-            .requires(1, AVPItems.RAW_BAUXITE)
-            .requires(1, AVPItems.CARBON_DUST)
-            .requires(1, Items.RAW_IRON)
-            .into(2, AVPItems.RAW_FERROBAUXITE);
+                .withCategory(RecipeCategory.MISC)
+                .requires(1, AVPItems.RAW_BAUXITE)
+                .requires(1, AVPItems.CARBON_DUST)
+                .requires(1, Items.RAW_IRON)
+                .into(2, AVPItems.RAW_FERROBAUXITE);
 
         // Steel can only be blasted.
         builder.blast(AVPItems.RAW_CRUDE_IRON)
-            .withCategory(RecipeCategory.MISC)
-            .withExperience(RecipeConstants.RARE_SMELT_EXPERIENCE)
-            .into(AVPItems.STEEL_INGOT);
+                .withCategory(RecipeCategory.MISC)
+                .withExperience(RecipeConstants.RARE_SMELT_EXPERIENCE)
+                .into(AVPItems.STEEL_INGOT);
 
         builder.shapeless()
-            .withCategory(RecipeCategory.MISC)
-            .requires(1, AVPItems.RAW_ZINC)
-            .requires(1, Items.RAW_COPPER)
-            .into(2, AVPItems.RAW_BRASS);
+                .withCategory(RecipeCategory.MISC)
+                .requires(1, AVPItems.RAW_ZINC)
+                .requires(1, Items.RAW_COPPER)
+                .into(2, AVPItems.RAW_BRASS);
 
         builder.shaped()
-            .withCategory(RecipeCategory.MISC)
-            .define('A', AVPItems.AUTUNITE_DUST)
-            .define('T', AVPItems.TITANIUM_INGOT)
-            .pattern(" A ")
-            .pattern("ATA")
-            .pattern(" A ")
-            .into(1, AVPItems.URANIUM_INGOT);
+                .withCategory(RecipeCategory.MISC)
+                .define('A', AVPItems.AUTUNITE_DUST)
+                .define('T', AVPItems.TITANIUM_INGOT)
+                .pattern(" A ")
+                .pattern("ATA")
+                .pattern(" A ")
+                .into(1, AVPItems.URANIUM_INGOT);
 
 
 
@@ -142,9 +142,22 @@ public class MetalRecipeProvider {
                 .apply(RecipeTemplates.PRESSURE_PLATE_BLOCK.apply(AVPItems.TITANIUM_INGOT))
                 .into(1, AVPBlocks.TITANIUM_PRESSURE_PLATE);
 
+        // Add standard slab and stair crafting recipes
+        createStandardSlabRecipe(builder.shaped(), AVPBlocks.FERROALUMINUM_BLOCK, AVPBlocks.FERROALUMINUM_SLAB);
+        createStandardStairRecipe(builder.shaped(), AVPBlocks.FERROALUMINUM_BLOCK, AVPBlocks.FERROALUMINUM_STAIRS);
+
+        createStandardSlabRecipe(builder.shaped(), AVPBlocks.STEEL_BLOCK, AVPBlocks.STEEL_SLAB);
+        createStandardStairRecipe(builder.shaped(), AVPBlocks.STEEL_BLOCK, AVPBlocks.STEEL_STAIRS);
+
+        createStandardSlabRecipe(builder.shaped(), AVPBlocks.TITANIUM_BLOCK, AVPBlocks.TITANIUM_SLAB);
+        createStandardStairRecipe(builder.shaped(), AVPBlocks.TITANIUM_BLOCK, AVPBlocks.TITANIUM_STAIRS);
+
         createFerroaluminumBlockVariantRecipes(builder);
         createSteelBlockVariantRecipes(builder);
         createTitaniumBlockVariantRecipes(builder);
+
+        // Add variant slab and stair recipes
+        createVariantSlabAndStairRecipes(builder);
 
         // Nugget to ingot recipes
         nuggetToIngot(builder.shaped(), AVPItems.FERROALUMINUM_NUGGET, AVPItems.FERROALUMINUM_INGOT);
@@ -162,14 +175,14 @@ public class MetalRecipeProvider {
         ingotToNugget(builder.shapeless(), AVPItems.BRASS_INGOT, AVPItems.BRASS_NUGGET);
         ingotToNugget(builder.shapeless(), AVPItems.TITANIUM_INGOT, AVPItems.TITANIUM_NUGGET);
         ingotToNugget(builder.shapeless(), AVPItems.LEAD_INGOT, AVPItems.LEAD_NUGGET);
-        //ingotToNugget(builder.shapeless(), AVPItems.URANIUM_INGOT, AVPItems.URANIUM_NUGGET);
+        ingotToNugget(builder.shapeless(), AVPItems.URANIUM_INGOT, AVPItems.URANIUM_NUGGET);
         ingotToNugget(builder.shapeless(), AVPItems.ZINC_INGOT, AVPItems.ZINC_NUGGET);
         ingotToNugget(builder.shapeless(), AVPItems.ALUMINUM_INGOT, AVPItems.ALUMINUM_NUGGET);
     }
 
     private static void createFerroaluminumBlockVariantRecipes(RecipeBuilder builder) {
         var ferroaluminumBaseBuilder = builder.stonecut(AVPBlocks.FERROALUMINUM_BLOCK)
-            .withCategory(RecipeCategory.BUILDING_BLOCKS);
+                .withCategory(RecipeCategory.BUILDING_BLOCKS);
 
         ferroaluminumBaseBuilder.into(4, AVPBlocks.CHISELED_FERROALUMINUM);
         ferroaluminumBaseBuilder.into(4, AVPBlocks.CUT_FERROALUMINUM);
@@ -184,9 +197,11 @@ public class MetalRecipeProvider {
         ferroaluminumBaseBuilder.into(4, AVPBlocks.FERROALUMINUM_SIDING);
         ferroaluminumBaseBuilder.into(4, AVPBlocks.FERROALUMINUM_STANDING);
         ferroaluminumBaseBuilder.into(4, AVPBlocks.FERROALUMINUM_TREAD);
+        ferroaluminumBaseBuilder.into(2, AVPBlocks.FERROALUMINUM_SLAB);
+        ferroaluminumBaseBuilder.into(1, AVPBlocks.FERROALUMINUM_STAIRS);
 
         var cutFerroaluminumBuilder = builder.stonecut(AVPBlocks.CUT_FERROALUMINUM)
-            .withCategory(RecipeCategory.BUILDING_BLOCKS);
+                .withCategory(RecipeCategory.BUILDING_BLOCKS);
 
         cutFerroaluminumBuilder.into(2, AVPBlocks.CUT_FERROALUMINUM_SLAB);
         cutFerroaluminumBuilder.into(1, AVPBlocks.CUT_FERROALUMINUM_STAIRS);
@@ -194,7 +209,7 @@ public class MetalRecipeProvider {
 
     private static void createSteelBlockVariantRecipes(RecipeBuilder builder) {
         var steelBaseBuilder = builder.stonecut(AVPBlocks.STEEL_BLOCK)
-            .withCategory(RecipeCategory.BUILDING_BLOCKS);
+                .withCategory(RecipeCategory.BUILDING_BLOCKS);
 
         steelBaseBuilder.into(4, AVPBlocks.CHISELED_STEEL);
         steelBaseBuilder.into(4, AVPBlocks.CUT_STEEL);
@@ -209,9 +224,11 @@ public class MetalRecipeProvider {
         steelBaseBuilder.into(4, AVPBlocks.STEEL_SIDING);
         steelBaseBuilder.into(4, AVPBlocks.STEEL_STANDING);
         steelBaseBuilder.into(4, AVPBlocks.STEEL_TREAD);
+        steelBaseBuilder.into(2, AVPBlocks.STEEL_SLAB);
+        steelBaseBuilder.into(1, AVPBlocks.STEEL_STAIRS);
 
         var cutSteelBuilder = builder.stonecut(AVPBlocks.CUT_STEEL)
-            .withCategory(RecipeCategory.BUILDING_BLOCKS);
+                .withCategory(RecipeCategory.BUILDING_BLOCKS);
 
         cutSteelBuilder.into(2, AVPBlocks.CUT_STEEL_SLAB);
         cutSteelBuilder.into(1, AVPBlocks.CUT_STEEL_STAIRS);
@@ -219,7 +236,7 @@ public class MetalRecipeProvider {
 
     private static void createTitaniumBlockVariantRecipes(RecipeBuilder builder) {
         var titaniumBaseBuilder = builder.stonecut(AVPBlocks.TITANIUM_BLOCK)
-            .withCategory(RecipeCategory.BUILDING_BLOCKS);
+                .withCategory(RecipeCategory.BUILDING_BLOCKS);
 
         titaniumBaseBuilder.into(4, AVPBlocks.CHISELED_TITANIUM);
         titaniumBaseBuilder.into(4, AVPBlocks.CUT_TITANIUM);
@@ -234,24 +251,167 @@ public class MetalRecipeProvider {
         titaniumBaseBuilder.into(4, AVPBlocks.TITANIUM_SIDING);
         titaniumBaseBuilder.into(4, AVPBlocks.TITANIUM_STANDING);
         titaniumBaseBuilder.into(4, AVPBlocks.TITANIUM_TREAD);
+        titaniumBaseBuilder.into(2, AVPBlocks.TITANIUM_SLAB);
+        titaniumBaseBuilder.into(1, AVPBlocks.TITANIUM_STAIRS);
 
         var cutTitaniumBuilder = builder.stonecut(AVPBlocks.CUT_TITANIUM)
-            .withCategory(RecipeCategory.BUILDING_BLOCKS);
+                .withCategory(RecipeCategory.BUILDING_BLOCKS);
 
         cutTitaniumBuilder.into(2, AVPBlocks.CUT_TITANIUM_SLAB);
         cutTitaniumBuilder.into(1, AVPBlocks.CUT_TITANIUM_STAIRS);
     }
 
+    private static void createVariantSlabAndStairRecipes(RecipeBuilder builder) {
+        // Ferroaluminum variant slabs and stairs
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.FERROALUMINUM_SIDING,
+                AVPBlocks.FERROALUMINUM_SIDING_SLAB,
+                AVPBlocks.FERROALUMINUM_SIDING_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.FERROALUMINUM_STANDING,
+                AVPBlocks.FERROALUMINUM_STANDING_SLAB,
+                AVPBlocks.FERROALUMINUM_STANDING_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.FERROALUMINUM_FASTENED_SIDING,
+                AVPBlocks.FERROALUMINUM_FASTENED_SIDING_SLAB,
+                AVPBlocks.FERROALUMINUM_FASTENED_SIDING_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.FERROALUMINUM_FASTENED_STANDING,
+                AVPBlocks.FERROALUMINUM_FASTENED_STANDING_SLAB,
+                AVPBlocks.FERROALUMINUM_FASTENED_STANDING_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.FERROALUMINUM_PLATING,
+                AVPBlocks.FERROALUMINUM_PLATING_SLAB,
+                AVPBlocks.FERROALUMINUM_PLATING_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.FERROALUMINUM_TREAD,
+                AVPBlocks.FERROALUMINUM_TREAD_SLAB,
+                AVPBlocks.FERROALUMINUM_TREAD_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.FERROALUMINUM_GRATE,
+                AVPBlocks.FERROALUMINUM_GRATE_SLAB,
+                AVPBlocks.FERROALUMINUM_GRATE_STAIRS);
+
+        // Steel variant slabs and stairs
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.STEEL_SIDING,
+                AVPBlocks.STEEL_SIDING_SLAB,
+                AVPBlocks.STEEL_SIDING_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.STEEL_STANDING,
+                AVPBlocks.STEEL_STANDING_SLAB,
+                AVPBlocks.STEEL_STANDING_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.STEEL_FASTENED_SIDING,
+                AVPBlocks.STEEL_FASTENED_SIDING_SLAB,
+                AVPBlocks.STEEL_FASTENED_SIDING_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.STEEL_FASTENED_STANDING,
+                AVPBlocks.STEEL_FASTENED_STANDING_SLAB,
+                AVPBlocks.STEEL_FASTENED_STANDING_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.STEEL_PLATING,
+                AVPBlocks.STEEL_PLATING_SLAB,
+                AVPBlocks.STEEL_PLATING_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.STEEL_TREAD,
+                AVPBlocks.STEEL_TREAD_SLAB,
+                AVPBlocks.STEEL_TREAD_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.STEEL_GRATE,
+                AVPBlocks.STEEL_GRATE_SLAB,
+                AVPBlocks.STEEL_GRATE_STAIRS);
+
+        // Titanium variant slabs and stairs
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.TITANIUM_SIDING,
+                AVPBlocks.TITANIUM_SIDING_SLAB,
+                AVPBlocks.TITANIUM_SIDING_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.TITANIUM_STANDING,
+                AVPBlocks.TITANIUM_STANDING_SLAB,
+                AVPBlocks.TITANIUM_STANDING_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.TITANIUM_FASTENED_SIDING,
+                AVPBlocks.TITANIUM_FASTENED_SIDING_SLAB,
+                AVPBlocks.TITANIUM_FASTENED_SIDING_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.TITANIUM_FASTENED_STANDING,
+                AVPBlocks.TITANIUM_FASTENED_STANDING_SLAB,
+                AVPBlocks.TITANIUM_FASTENED_STANDING_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.TITANIUM_PLATING,
+                AVPBlocks.TITANIUM_PLATING_SLAB,
+                AVPBlocks.TITANIUM_PLATING_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.TITANIUM_TREAD,
+                AVPBlocks.TITANIUM_TREAD_SLAB,
+                AVPBlocks.TITANIUM_TREAD_STAIRS);
+
+        addVariantSlabAndStairRecipes(builder,
+                AVPBlocks.TITANIUM_GRATE,
+                AVPBlocks.TITANIUM_GRATE_SLAB,
+                AVPBlocks.TITANIUM_GRATE_STAIRS);
+    }
+
+    private static void addVariantSlabAndStairRecipes(RecipeBuilder builder, ItemLike baseBlock, ItemLike slab, ItemLike stairs) {
+        // Add stonecut recipes for the block variants
+        builder.stonecut(baseBlock)
+                .withCategory(RecipeCategory.BUILDING_BLOCKS)
+                .into(2, slab);
+
+        builder.stonecut(baseBlock)
+                .withCategory(RecipeCategory.BUILDING_BLOCKS)
+                .into(1, stairs);
+
+        // Add shaped crafting recipes
+        createStandardSlabRecipe(builder.shaped(), baseBlock, slab);
+        createStandardStairRecipe(builder.shaped(), baseBlock, stairs);
+    }
+
+    private static void createStandardSlabRecipe(ShapedRecipeBuilder builder, ItemLike input, ItemLike output) {
+        builder.withCategory(RecipeCategory.BUILDING_BLOCKS)
+                .define('#', input)
+                .pattern("###")
+                .into(6, output);
+    }
+
+    private static void createStandardStairRecipe(ShapedRecipeBuilder builder, ItemLike input, ItemLike output) {
+        builder.withCategory(RecipeCategory.BUILDING_BLOCKS)
+                .define('#', input)
+                .pattern("#  ")
+                .pattern("## ")
+                .pattern("###")
+                .into(4, output);
+    }
+
     private static void createSmeltAndBlastRecipes(RecipeBuilder builder, ItemLike input, ItemLike output) {
         builder.smelt(input)
-            .withCategory(RecipeCategory.MISC)
-            .withExperience(RecipeConstants.RARE_SMELT_EXPERIENCE)
-            .into(output);
+                .withCategory(RecipeCategory.MISC)
+                .withExperience(RecipeConstants.RARE_SMELT_EXPERIENCE)
+                .into(output);
 
         builder.blast(input)
-            .withCategory(RecipeCategory.MISC)
-            .withExperience(RecipeConstants.RARE_SMELT_EXPERIENCE)
-            .into(output);
+                .withCategory(RecipeCategory.MISC)
+                .withExperience(RecipeConstants.RARE_SMELT_EXPERIENCE)
+                .into(output);
     }
 
     private static void nuggetToIngot(ShapedRecipeBuilder builder, ItemLike input, ItemLike output)
