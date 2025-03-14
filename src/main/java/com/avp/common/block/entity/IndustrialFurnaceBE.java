67ef20ae4a78e85a6d99e9498b69cdebd5f3f7d2
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-03-14 18:49:41 -0400

    Update IndustrialFurnaceBE.java

--- src/main/java/com/avp/common/block/entity/IndustrialFurnaceBE.java
+++ src/main/java/com/avp/common/block/entity/IndustrialFurnaceBE.java
@@ -10,7 +10,6 @@ import net.minecraft.world.inventory.BlastFurnaceMenu;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeHolder;
-import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.item.crafting.SingleRecipeInput;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.AbstractFurnaceBlock;
