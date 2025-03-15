0f42250bebc81122ae35ffaa5656b6d884fd0565
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-03-14 21:46:32 -0400

    Makes razor wire not a pathfindable block

--- src/main/java/com/avp/common/block/RazorWireBlock.java
+++ src/main/java/com/avp/common/block/RazorWireBlock.java
@@ -10,6 +10,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -25,6 +26,11 @@ public class RazorWireBlock extends Block {
 
     public RazorWireBlock(BlockBehaviour.Properties properties) {
         super(properties);
+    }
+
+    @Override
+    protected boolean isPathfindable(BlockState blockState, PathComputationType pathComputationType) {
+        return false;
     }
 
     @Override
