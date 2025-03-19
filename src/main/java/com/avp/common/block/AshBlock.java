b94365d0c07c3d115f47b7c861c492475e9abf68
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-03-19 13:37:12 -0400

    Updates Trinitite block texture

---
+++ src/main/java/com/avp/common/block/AshBlock.java
@@ -0,0 +1,46 @@
+package com.avp.common.block;
+
+import com.avp.common.worldgen.biome.AVPBiomes;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.SnowLayerBlock;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class AshBlock extends SnowLayerBlock {
+
+    public AshBlock(Properties properties) {
+        super(properties);
+        this.registerDefaultState((this.stateDefinition.any()).setValue(LAYERS, 1));
+    }
+
+    @Override
+    protected boolean canSurvive(BlockState blockState, LevelReader levelReader, BlockPos blockPos) {
+        return levelReader.getBiome(blockPos).is(AVPBiomes.NUKED_BIOME);
+    }
+
+    @Override
+    protected void randomTick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, RandomSource randomSource) {
+        if (randomSource.nextInt(1) == 0) {
+            if (blockState.getValue(LAYERS) < 8) {
+                serverLevel.setBlockAndUpdate(blockPos,
+                        this.defaultBlockState().setValue(LAYERS, blockState.getValue(LAYERS) + 1));
+            } else {
+                for (var dx = -1; dx <= 1; dx++) {
+                    for (var dz = -1; dz <= 1; dz++) {
+                        var targetPos = blockPos.offset(dx, 0, dz);
+                        var targetPosBelow = targetPos.below();
+
+                        if (serverLevel.getBlockState(targetPos).isAir() &&
+                                serverLevel.getBlockState(targetPosBelow).isSolidRender(serverLevel, targetPosBelow)) {
+                            serverLevel.setBlockAndUpdate(targetPos,
+                                    this.defaultBlockState().setValue(LAYERS, 1));
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
