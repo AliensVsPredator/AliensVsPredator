bfc84ad88254cc4c2b07d7baf91e9e548b71231e
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-03-25 10:00:39 -0400

    Predators no longer do a light a check to spawn

--- src/main/java/com/avp/common/entity/living/yautja/YautjaSpawning.java
+++ src/main/java/com/avp/common/entity/living/yautja/YautjaSpawning.java
@@ -2,7 +2,9 @@ package com.avp.common.entity.living.yautja;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.Difficulty;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.entity.monster.Monster;
@@ -34,12 +36,7 @@ public class YautjaSpawning {
         BlockPos blockPos,
         RandomSource randomSource
     ) {
-        return Monster.checkMonsterSpawnRules(
-            entityType,
-            serverLevelAccessor,
-            mobSpawnType,
-            blockPos,
-            randomSource
-        );
+        return serverLevelAccessor.getDifficulty() != Difficulty.PEACEFUL
+                && Mob.checkMobSpawnRules(entityType, serverLevelAccessor, mobSpawnType, blockPos, randomSource);
     }
 }
