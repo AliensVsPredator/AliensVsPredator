78c0c68bcb51db87cee05651b91cc71c70fab8fa
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-03-28 04:04:12 -0400

    Adds in REI support

---
+++ src/main/java/com/avp/client/rei/IndustrialDisplay.java
@@ -0,0 +1,24 @@
+package com.avp.client.rei;
+
+import com.avp.common.recipe.IndustrialFurnaceRecipe;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
+import me.shedaniel.rei.api.common.entry.EntryIngredient;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
+import me.shedaniel.rei.api.common.util.EntryStacks;
+import net.minecraft.world.item.crafting.RecipeHolder;
+
+import java.util.List;
+
+public class IndustrialDisplay extends BasicDisplay {
+
+    public IndustrialDisplay(RecipeHolder<IndustrialFurnaceRecipe> recipe) {
+        super(List.of(EntryIngredients.ofIngredient(recipe.value().getIngredients().get(0))),
+                List.of(EntryIngredient.of(EntryStacks.of(recipe.value().getResultItem(null)))));
+    }
+
+    @Override
+    public CategoryIdentifier<?> getCategoryIdentifier() {
+        return IndustrialCategory.INDUSTRIAL_FURNACE;
+    }
+}
