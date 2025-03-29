ab93130d6b0afaf3e33649a489df821c82e526dc
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-03-29 15:22:31 -0400

    Desk Terminal now has facing values set

--- src/main/java/com/avp/common/block/DeskTerminalBlock.java
+++ src/main/java/com/avp/common/block/DeskTerminalBlock.java
@@ -4,9 +4,16 @@ import com.avp.common.block.base.BaseBlockEntity;
 import com.avp.common.block.entity.DeskTerminalBE;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DirectionalBlock;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -14,8 +21,26 @@ public class DeskTerminalBlock extends BaseBlockEntity {
 
     public static final MapCodec<DeskTerminalBlock> CODEC = simpleCodec(DeskTerminalBlock::new);
 
+    public static final DirectionProperty FACING = DirectionalBlock.FACING;
+
     public DeskTerminalBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected @NotNull BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
     }
 
     @Override
