68a732188a9ad2d0a7455c1a7ba0715508e6c18d
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-04-05 16:36:49 -0400

    🐞 MK50 Rendering brown instead of white when undyed

--- src/main/java/com/avp/client/render/armor/MK50ArmorRenderer.java
+++ src/main/java/com/avp/client/render/armor/MK50ArmorRenderer.java
@@ -1,11 +1,20 @@
 package com.avp.client.render.armor;
 
+import mod.azure.azurelib.core.object.Color;
+import mod.azure.azurelib.rewrite.render.AzRendererConfig;
+import mod.azure.azurelib.rewrite.render.AzRendererPipeline;
+import mod.azure.azurelib.rewrite.render.AzRendererPipelineContext;
 import mod.azure.azurelib.rewrite.render.armor.AzArmorRenderer;
 import mod.azure.azurelib.rewrite.render.armor.AzArmorRendererConfig;
+import mod.azure.azurelib.rewrite.render.armor.AzArmorRendererPipeline;
+import mod.azure.azurelib.rewrite.render.armor.AzArmorRendererPipelineContext;
 import net.minecraft.resources.ResourceLocation;
 
 import com.avp.AVPResources;
 import com.avp.client.render.layer.MKOuterLayer;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.DyedItemColor;
 
 public class MK50ArmorRenderer extends AzArmorRenderer {
 
@@ -21,5 +30,23 @@ public class MK50ArmorRenderer extends AzArmorRenderer {
                 .addRenderLayer(new MKOuterLayer())
                 .build()
         );
+    }
+
+    @Override
+    protected AzArmorRendererPipeline createPipeline(AzRendererConfig config) {
+        return new AzArmorRendererPipeline(config, this) {
+            @Override
+            protected AzRendererPipelineContext<ItemStack> createContext(AzRendererPipeline<ItemStack> rendererPipeline) {
+                return new AzArmorRendererPipelineContext(rendererPipeline) {
+                    @Override
+                    public Color getRenderColor(ItemStack animatable, float partialTick, int packedLight) {
+                        return this.currentStack().is(ItemTags.DYEABLE)
+                                ? Color.ofOpaque(
+                                DyedItemColor.getOrDefault(this.currentStack(), Color.WHITE.getColor()))
+                                : Color.WHITE;
+                    }
+                };
+            }
+        };
     }
 }
