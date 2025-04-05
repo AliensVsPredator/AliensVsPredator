ef8924700426729fca088df0070f1ed9efd74677
bvanseg <bvanseg@gmail.com>
2025-04-05 18:54:37 -0400

    🎨 Formatting.
    Signed-off-by: = <bvanseg@gmail.com>

--- src/main/java/com/avp/client/render/armor/MK50ArmorRenderer.java
+++ src/main/java/com/avp/client/render/armor/MK50ArmorRenderer.java
@@ -9,12 +9,12 @@ import mod.azure.azurelib.rewrite.render.armor.AzArmorRendererConfig;
 import mod.azure.azurelib.rewrite.render.armor.AzArmorRendererPipeline;
 import mod.azure.azurelib.rewrite.render.armor.AzArmorRendererPipelineContext;
 import net.minecraft.resources.ResourceLocation;
-
-import com.avp.AVPResources;
-import com.avp.client.render.layer.MKOuterLayer;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.DyedItemColor;
+
+import com.avp.AVPResources;
+import com.avp.client.render.layer.MKOuterLayer;
 
 public class MK50ArmorRenderer extends AzArmorRenderer {
 
@@ -35,15 +35,18 @@ public class MK50ArmorRenderer extends AzArmorRenderer {
     @Override
     protected AzArmorRendererPipeline createPipeline(AzRendererConfig config) {
         return new AzArmorRendererPipeline(config, this) {
+
             @Override
             protected AzRendererPipelineContext<ItemStack> createContext(AzRendererPipeline<ItemStack> rendererPipeline) {
                 return new AzArmorRendererPipelineContext(rendererPipeline) {
+
                     @Override
                     public Color getRenderColor(ItemStack animatable, float partialTick, int packedLight) {
                         return this.currentStack().is(ItemTags.DYEABLE)
-                                ? Color.ofOpaque(
-                                DyedItemColor.getOrDefault(this.currentStack(), Color.WHITE.getColor()))
-                                : Color.WHITE;
+                            ? Color.ofOpaque(
+                                DyedItemColor.getOrDefault(this.currentStack(), Color.WHITE.getColor())
+                            )
+                            : Color.WHITE;
                     }
                 };
             }
