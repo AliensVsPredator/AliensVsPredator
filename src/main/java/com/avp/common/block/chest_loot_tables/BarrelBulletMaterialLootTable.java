a12610d8373a8c422be79d3c2b31319b71a036a9
bvanseg <bvanseg@gmail.com>
2025-04-10 09:21:26 -0400

    🎨 Formatting.
    Signed-off-by: = <bvanseg@gmail.com>

--- src/main/java/com/avp/common/block/chest_loot_tables/BarrelBulletMaterialLootTable.java
+++ src/main/java/com/avp/common/block/chest_loot_tables/BarrelBulletMaterialLootTable.java
@@ -5,7 +5,6 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
-import net.minecraft.world.level.storage.loot.functions.EnchantedCountIncreaseFunction;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
