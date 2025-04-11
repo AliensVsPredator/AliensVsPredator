e03379f92d870f17a5c22f587fcbc639abc58271
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2025-04-11 11:35:03 -0300

    Add code to pickup powder snow

--- src/main/java/com/avp/common/item/SolidCanisterItem.java
+++ src/main/java/com/avp/common/item/SolidCanisterItem.java
@@ -1,8 +1,13 @@
 package com.avp.common.item;
 
 import com.avp.common.component.DataComponents;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
@@ -10,8 +15,13 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BucketPickup;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -37,8 +47,9 @@ public class SolidCanisterItem extends BlockItem {
     @Override
     public @NotNull InteractionResult useOn(UseOnContext context) {
         InteractionResult interactionResult = super.useOn(context);
+
         Player player = context.getPlayer();
-        if (interactionResult.consumesAction() && player != null) {
+        if (interactionResult.consumesAction() && player != null && player.isShiftKeyDown()) {
             if (context.getItemInHand().getOrDefault(DataComponents.CANISTER_CONTENT_AMOUNT, 0) > 1 && !player.isCreative()) {
                 CanisterItem.updateContentAmount(context.getItemInHand(), -1);
                 return interactionResult;
@@ -46,8 +57,47 @@ public class SolidCanisterItem extends BlockItem {
 
             player.setItemInHand(context.getHand(), CanisterItem.getEmptySuccessItem(context.getItemInHand(), player));
         }
+        else if (player != null) {
+            ItemStack canisterStack = context.getItemInHand();
+            BlockHitResult hitResult = getPlayerPOVHitResult(context.getLevel(), player, ClipContext.Fluid.SOURCE_ONLY);
 
-        return interactionResult;
+            if (CanisterItem.isInvalidHitResult(hitResult))
+                return InteractionResult.PASS;
+
+            BlockPos hitPos = hitResult.getBlockPos();
+            Direction hitDir = hitResult.getDirection();
+            BlockPos relativePos = hitPos.relative(hitDir);
+
+            if (!CanisterItem.canPlayerInteract(context.getLevel(), player, hitPos, relativePos, hitDir, canisterStack))
+                return InteractionResult.FAIL;
+
+            BlockState hitState = context.getLevel().getBlockState(hitPos);
+
+            if (CanisterItem.isFluidPickupAction(player, hitState))
+                return handlePowderSnowPickup(player, context.getLevel(), canisterStack, hitPos, hitState);
+        }
+
+        return InteractionResult.FAIL;
+    }
+
+    private InteractionResult handlePowderSnowPickup(Player player, Level level, ItemStack canisterStack, BlockPos hitPos, BlockState hitState) {
+        BucketPickup bucketPickup = (BucketPickup) hitState.getBlock();
+
+        if (canisterStack.getOrDefault(DataComponents.CANISTER_CONTENT_AMOUNT, 0) < CanisterItem.MAX_CONTENT_AMOUNT) {
+            bucketPickup.pickupBlock(player, level, hitPos, hitState);
+
+            player.awardStat(Stats.ITEM_USED.get(this));
+            bucketPickup.getPickupSound().ifPresent(sound -> player.playSound(sound, 1.0F, 1.0F));
+            level.gameEvent(player, GameEvent.FLUID_PICKUP, hitPos);
+
+            CanisterItem.updateContentAmount(canisterStack, 1);
+
+            if (!level.isClientSide)
+                CriteriaTriggers.FILLED_BUCKET.trigger((ServerPlayer) player, canisterStack);
+
+            return InteractionResult.sidedSuccess(level.isClientSide);
+        }
+        return InteractionResult.FAIL;
     }
 
     @Override
