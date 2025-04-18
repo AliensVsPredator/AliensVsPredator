4269042e2b776e642c100854c5469bba6f856ef2
bvanseg <bvanseg@gmail.com>
2025-04-18 19:47:35 -0400

    ♻️ Added GOAPMutableWorldState#apply.
    Signed-off-by: = <bvanseg@gmail.com>

--- src/main/java/com/avp/goap/state/GOAPMutableWorldState.java
+++ src/main/java/com/avp/goap/state/GOAPMutableWorldState.java
@@ -10,11 +10,19 @@ public class GOAPMutableWorldState extends GOAPWorldState {
         this(new HashMap<>());
     }
 
+    public GOAPMutableWorldState(GOAPWorldState worldState) {
+        this(new HashMap<>(worldState.stateMap));
+    }
+
     public GOAPMutableWorldState(HashMap<TypedIdentifier<?>, Object> stateMap) {
         super(stateMap);
     }
 
     public <T> void set(TypedIdentifier<T> key, T value) {
         stateMap.put(key, value);
+    }
+
+    public void apply(GOAPWorldState worldState) {
+        stateMap.putAll(worldState.stateMap);
     }
 }
