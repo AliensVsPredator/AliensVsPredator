e59c7a645792c163d4dad4ef46fdccd6bbd85cea
bvanseg <bvanseg@gmail.com>
2025-04-18 12:10:08 -0400

    ✨ Added EntityGOAP type.
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ src/main/java/com/avp/common/entity/ai/EntityGOAP.java
@@ -0,0 +1,17 @@
+package com.avp.common.entity.ai;
+
+import net.minecraft.world.entity.LivingEntity;
+
+import com.avp.goap.GOAP;
+
+public abstract class EntityGOAP<T extends LivingEntity> extends GOAP<T> {
+
+    @Override
+    public void update(T context) {
+        if (!context.isAlive() || context.isDeadOrDying()) {
+            return;
+        }
+
+        super.update(context);
+    }
+}
