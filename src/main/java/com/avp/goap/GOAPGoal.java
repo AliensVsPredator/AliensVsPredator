779bb51d63a61f9dadc6d7721ba53da092a63780
bvanseg <bvanseg@gmail.com>
2025-04-18 19:47:52 -0400

    ♻️ Refactor GOAPGoal, override toString.
    Signed-off-by: = <bvanseg@gmail.com>

--- src/main/java/com/avp/goap/GOAPGoal.java
+++ src/main/java/com/avp/goap/GOAPGoal.java
@@ -4,22 +4,27 @@ import com.avp.goap.expression.GOAPConditionSet;
 
 public abstract class GOAPGoal {
 
-    private final GOAPConditionSet desiredWorldState;
+    private final GOAPConditionSet desiredConditions;
 
     private final String name;
 
     protected GOAPGoal() {
-        this.desiredWorldState = createDesiredConditions();
+        this.desiredConditions = createDesiredConditions();
         this.name = this.getClass().getSimpleName();
     }
 
-    public abstract GOAPConditionSet createDesiredConditions();
+    protected abstract GOAPConditionSet createDesiredConditions();
 
-    public GOAPConditionSet getDesiredWorldState() {
-        return desiredWorldState;
+    public GOAPConditionSet getDesiredConditions() {
+        return desiredConditions;
     }
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public String toString() {
+        return getName();
     }
 }
