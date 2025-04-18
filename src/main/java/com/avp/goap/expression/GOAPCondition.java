b5afcf8e2333e1edeb1dd7ed93f9cdea7be381e0
bvanseg <bvanseg@gmail.com>
2025-04-18 19:24:32 -0400

    ♻️ Added GOAPCondition#satisfiedBy.
    Signed-off-by: = <bvanseg@gmail.com>

--- src/main/java/com/avp/goap/expression/GOAPCondition.java
+++ src/main/java/com/avp/goap/expression/GOAPCondition.java
@@ -1,8 +1,16 @@
 package com.avp.goap.expression;
 
 import com.avp.goap.TypedIdentifier;
+import com.avp.goap.state.GOAPWorldState;
 
 public record GOAPCondition<T>(
     TypedIdentifier<? super T> identifier,
     GOAPExpression<? super T> expression
-) {}
+) {
+
+    public boolean satisfiedBy(GOAPWorldState worldState) {
+        @SuppressWarnings("unchecked")
+        var value = (T) worldState.get(identifier);
+        return value != null && expression.evaluate(value);
+    }
+}
