0235a849d1754f60b72f5e472164765049e243ff
bvanseg <bvanseg@gmail.com>
2025-04-20 10:47:40 -0400

    ♻️ Changed GOAPCondition#identifier typing for better type safety.
    Signed-off-by: = <bvanseg@gmail.com>

--- src/main/java/com/avp/goap/expression/GOAPCondition.java
+++ src/main/java/com/avp/goap/expression/GOAPCondition.java
@@ -4,12 +4,11 @@ import com.avp.goap.TypedIdentifier;
 import com.avp.goap.state.GOAPWorldState;
 
 public record GOAPCondition<T>(
-    TypedIdentifier<? super T> identifier,
+    TypedIdentifier<? extends T> identifier,
     GOAPExpression<? super T> expression
 ) {
 
     public boolean satisfiedBy(GOAPWorldState worldState) {
-        @SuppressWarnings("unchecked")
         var value = (T) worldState.get(identifier);
         return value != null && expression.evaluate(value);
     }
