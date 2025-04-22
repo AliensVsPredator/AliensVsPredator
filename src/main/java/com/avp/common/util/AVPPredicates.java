fd45de9d768c503326e5fd1d567e2de8c76df27e
bvanseg <bvanseg@gmail.com>
2025-04-22 00:57:06 -0400

    🐞 Fixed nuke nuke command.
    Signed-off-by: = <bvanseg@gmail.com>

--- src/main/java/com/avp/common/util/AVPPredicates.java
+++ src/main/java/com/avp/common/util/AVPPredicates.java
@@ -1,6 +1,5 @@
 package com.avp.common.util;
 
-import net.minecraft.world.Container;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
