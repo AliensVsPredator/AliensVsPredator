499e9f661be85a38146e74999943650eb0f48452
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-05-02 11:40:51 -0400

    🛠Adds BiomeModifier keys for mob spawns for getting that closer

--- neoforge/src/main/java/com/avp/neoforge/data/AVPNeoForgeDatagen.java
+++ neoforge/src/main/java/com/avp/neoforge/data/AVPNeoForgeDatagen.java
@@ -1,21 +1,15 @@
 package com.avp.neoforge.data;
 
 import com.avp.AVP;
-import com.avp.AVPResources;
-import net.minecraft.core.HolderGetter;
-import net.minecraft.core.HolderSet;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.DataProvider;
-import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
-import net.neoforged.neoforge.common.world.BiomeModifier;
 import net.neoforged.neoforge.common.world.BiomeModifiers;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
@@ -26,11 +20,6 @@ import java.util.Set;
 @EventBusSubscriber(modid = AVP.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class AVPNeoForgeDatagen {
 
-    public static final ResourceKey<BiomeModifier> ADD_SPAWNS_EXAMPLE = ResourceKey.create(
-            NeoForgeRegistries.Keys.BIOME_MODIFIERS,
-            AVPResources.location("add_spawns_example")
-    );
-
     @SubscribeEvent
     public static void onGatherData(GatherDataEvent event) {
         event.getGenerator().addProvider(
@@ -40,12 +29,89 @@ public class AVPNeoForgeDatagen {
                 event.getLookupProvider(),
                 new RegistrySetBuilder()
                     .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, bootstrap -> {
-                        HolderGetter<Biome> biomes = bootstrap.lookup(Registries.BIOME);
-                        bootstrap.register(ADD_SPAWNS_EXAMPLE, new BiomeModifiers.AddSpawnsBiomeModifier(
-                            HolderSet.direct(biomes.getOrThrow(Biomes.PLAINS)),
+                        bootstrap.register(AVPEntitySpawnKeys.ADD_SPAWNS_OVAMORPH, new BiomeModifiers.AddSpawnsBiomeModifier(
+                                bootstrap.lookup(Registries.BIOME).getOrThrow(BiomeTags.IS_OVERWORLD),
+                                List.of(
+                                        new MobSpawnSettings.SpawnerData(EntityType.GHAST, 100, 1, 4)
+                                )
+                        ));
+                        bootstrap.register(AVPEntitySpawnKeys.ADD_SPAWNS_CHESTBURSTER, new BiomeModifiers.AddSpawnsBiomeModifier(
+                                bootstrap.lookup(Registries.BIOME).getOrThrow(BiomeTags.IS_OVERWORLD),
+                                List.of(
+                                        new MobSpawnSettings.SpawnerData(EntityType.GHAST, 100, 1, 4)
+                                )
+                        ));
+                        bootstrap.register(AVPEntitySpawnKeys.ADD_SPAWNS_DRONE, new BiomeModifiers.AddSpawnsBiomeModifier(
+                            bootstrap.lookup(Registries.BIOME).getOrThrow(BiomeTags.IS_OVERWORLD),
                             List.of(
-                                new MobSpawnSettings.SpawnerData(EntityType.GHAST, 100, 1, 4),
-                                new MobSpawnSettings.SpawnerData(EntityType.GHAST, 1, 5, 10)
+                                new MobSpawnSettings.SpawnerData(EntityType.GHAST, 100, 1, 4)
+                            )
+                        ));
+                        bootstrap.register(AVPEntitySpawnKeys.ADD_SPAWNS_WARRIOR, new BiomeModifiers.AddSpawnsBiomeModifier(
+                                bootstrap.lookup(Registries.BIOME).getOrThrow(BiomeTags.IS_OVERWORLD),
+                                List.of(
+                                        new MobSpawnSettings.SpawnerData(EntityType.GHAST, 100, 1, 4)
+                                )
+                        ));
+                        bootstrap.register(AVPEntitySpawnKeys.ADD_SPAWNS_PRAETORIAN, new BiomeModifiers.AddSpawnsBiomeModifier(
+                                bootstrap.lookup(Registries.BIOME).getOrThrow(BiomeTags.IS_OVERWORLD),
+                                List.of(
+                                        new MobSpawnSettings.SpawnerData(EntityType.GHAST, 100, 1, 4)
+                                )
+                        ));
+                        bootstrap.register(AVPEntitySpawnKeys.ADD_SPAWNS_QUEEN, new BiomeModifiers.AddSpawnsBiomeModifier(
+                                bootstrap.lookup(Registries.BIOME).getOrThrow(BiomeTags.IS_OVERWORLD),
+                                List.of(
+                                        new MobSpawnSettings.SpawnerData(EntityType.GHAST, 100, 1, 4)
+                                )
+                        ));
+                        bootstrap.register(AVPEntitySpawnKeys.ADD_SPAWNS_NETHER_OVAMORPH, new BiomeModifiers.AddSpawnsBiomeModifier(
+                            bootstrap.lookup(Registries.BIOME).getOrThrow(BiomeTags.IS_NETHER),
+                            List.of(
+                                    //FIXME: MOVE TO NETHER OVAMORPH
+                                new MobSpawnSettings.SpawnerData(EntityType.GHAST, 100, 1, 4)
+                            )
+                        ));
+                        bootstrap.register(AVPEntitySpawnKeys.ADD_SPAWNS_NETHER_CHESTBURSTER, new BiomeModifiers.AddSpawnsBiomeModifier(
+                                bootstrap.lookup(Registries.BIOME).getOrThrow(BiomeTags.IS_NETHER),
+                                List.of(
+                                        //FIXME: MOVE TO NETHER CHESTBURSTER
+                                        new MobSpawnSettings.SpawnerData(EntityType.GHAST, 100, 1, 4)
+                                )
+                        ));
+                        bootstrap.register(AVPEntitySpawnKeys.ADD_SPAWNS_NETHER_DRONE, new BiomeModifiers.AddSpawnsBiomeModifier(
+                                bootstrap.lookup(Registries.BIOME).getOrThrow(BiomeTags.IS_NETHER),
+                                List.of(
+                                        //FIXME: MOVE TO NETHER DRONE
+                                        new MobSpawnSettings.SpawnerData(EntityType.GHAST, 100, 1, 4)
+                                )
+                        ));
+                        bootstrap.register(AVPEntitySpawnKeys.ADD_SPAWNS_NETHER_WARRIOR, new BiomeModifiers.AddSpawnsBiomeModifier(
+                                bootstrap.lookup(Registries.BIOME).getOrThrow(BiomeTags.IS_NETHER),
+                                List.of(
+                                        //FIXME: MOVE TO NETHER WARRIOR
+                                        new MobSpawnSettings.SpawnerData(EntityType.GHAST, 100, 1, 4)
+                                )
+                        ));
+                        bootstrap.register(AVPEntitySpawnKeys.ADD_SPAWNS_NETHER_PRAETORIAN, new BiomeModifiers.AddSpawnsBiomeModifier(
+                                bootstrap.lookup(Registries.BIOME).getOrThrow(BiomeTags.IS_NETHER),
+                                List.of(
+                                        //FIXME: MOVE TO NETHER PRAETORIAN
+                                        new MobSpawnSettings.SpawnerData(EntityType.GHAST, 100, 1, 4)
+                                )
+                        ));
+                        bootstrap.register(AVPEntitySpawnKeys.ADD_SPAWNS_NETHER_QUEEN, new BiomeModifiers.AddSpawnsBiomeModifier(
+                                bootstrap.lookup(Registries.BIOME).getOrThrow(BiomeTags.IS_NETHER),
+                                List.of(
+                                        //FIXME: MOVE TO NETHER QUEEN
+                                        new MobSpawnSettings.SpawnerData(EntityType.GHAST, 100, 1, 4)
+                                )
+                        ));
+                        bootstrap.register(AVPEntitySpawnKeys.ADD_SPAWNS_YAUTJA, new BiomeModifiers.AddSpawnsBiomeModifier(
+                            bootstrap.lookup(Registries.BIOME).getOrThrow(BiomeTags.IS_JUNGLE),
+                            List.of(
+                                //FIXME: MOVE TO YAUTJA
+                                new MobSpawnSettings.SpawnerData(EntityType.GHAST, 100, 1, 4)
                             )
                         ));
                     }),
@@ -53,4 +119,6 @@ public class AVPNeoForgeDatagen {
             )
         );
     }
+
+    private AVPNeoForgeDatagen() {}
 }
