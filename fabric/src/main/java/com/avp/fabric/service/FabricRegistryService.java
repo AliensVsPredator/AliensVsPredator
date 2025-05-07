140019a1bbc3bdc1c5ceb2b7bcefdf3dc0fbadc8
bvanseg <bvanseg@gmail.com>
2025-05-07 08:49:42 -0400

    🐞 Fixed villagers not becoming commissaries in Fabric. Fixes #45.
    Signed-off-by: = <bvanseg@gmail.com>

--- fabric/src/main/java/com/avp/fabric/service/FabricRegistryService.java
+++ fabric/src/main/java/com/avp/fabric/service/FabricRegistryService.java
@@ -7,19 +7,23 @@ import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
+import net.fabricmc.fabric.api.object.builder.v1.world.poi.PointOfInterestHelper;
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.ItemLike;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,9 +49,28 @@ public class FabricRegistryService implements RegistryService {
 
     @Override
     public <T> AVPDeferredHolder<T> register(Registry<? super T> registry, String id, Supplier<? extends T> supplier) {
-        var reference = Registry.registerForHolder(registry, AVPResources.location(id), supplier.get());
+        var object = supplier.get();
+
+        if (object instanceof PoiType poiType) {
+            // We have to do special handling for PoiType registration on the Fabric side, since Fabric wants
+            // Poi registrations to go through their "PointOfInterestHelper" type.
+            return registerPoiType(id, poiType);
+        }
+
+        var reference = Registry.registerForHolder(registry, AVPResources.location(id), object);
         @SuppressWarnings("unchecked")
         var holder = (Holder<T>) reference;
+        return new AVPDeferredHolder<>(holder::value, () -> holder);
+    }
+
+    private <T> @NotNull AVPDeferredHolder<T> registerPoiType(String id, PoiType poiType) {
+        var location = AVPResources.location(id);
+        PointOfInterestHelper.register(location, poiType.maxTickets(), poiType.validRange(), poiType.matchingStates());
+        // Immediately get the holder or throw. This should be safe to do since we registered the PoiType in the last
+        // line. This is necessary because PointOfInterestHelper doesn't return back a holder (which we need for
+        // AVPDeferredHolder) after registration.
+        @SuppressWarnings("unchecked")
+        var holder = (Holder<T>) BuiltInRegistries.POINT_OF_INTEREST_TYPE.getHolder(location).orElseThrow();
         return new AVPDeferredHolder<>(holder::value, () -> holder);
     }
 
