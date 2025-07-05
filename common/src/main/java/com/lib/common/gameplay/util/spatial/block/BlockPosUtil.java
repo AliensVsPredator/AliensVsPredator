f37633a21473e47f8251b596c8d53a6701a85b1f
bvanseg <bvanseg@gmail.com>
2025-07-04 23:49:22 -0400

    ✨ Added BlockPosUtil#getNeighborsMatching.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/com/lib/common/gameplay/util/spatial/block/BlockPosUtil.java
+++ common/src/main/java/com/lib/common/gameplay/util/spatial/block/BlockPosUtil.java
@@ -5,9 +5,29 @@ import net.minecraft.core.Direction;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class BlockPosUtil {
+
+    public static List<BlockPos> getNeighborsMatching(Level level, BlockPos centerPos, Predicate<BlockState> predicate) {
+        var matchingNeighbors = new ArrayList<BlockPos>();
+
+        for (var direction : Direction.values()) {
+            var neighborPos = centerPos.relative(direction);
+            var state = level.getBlockState(neighborPos);
+
+            if (predicate.test(state)) {
+                matchingNeighbors.add(neighborPos);
+            }
+        }
+
+        return matchingNeighbors;
+    }
 
     public static boolean isFireAdjacent(Level level, BlockPos blockPos) {
         return Direction.stream().anyMatch(direction -> level.getBlockState(blockPos.relative(direction)).is(BlockTags.FIRE));
