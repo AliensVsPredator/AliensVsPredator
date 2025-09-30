623556fe43f07126616d1a004ed68adccdf445d1
bvanseg <bvanseg@gmail.com>
2025-09-30 01:19:27 -0400

    ♻️ Potion drinking now takes time, abstracted DrinkPotionAction into ConsumeItemAction.
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ common/src/main/java/com/human/common/gameplay/entity/living/human/marine/ai/action/ConsumeItemAction.java
@@ -0,0 +1,34 @@
+package com.human.common.gameplay.entity.living.human.marine.ai.action;
+
+import com.avp.common.model.inventory.AVPInventoryHolder;
+import com.just.core.functional.function.predicate.Predicate3;
+import com.just.goap.GOAPKey;
+import com.just.goap.state.Blackboard;
+import com.just.goap.state.ReadableWorldState;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.entity.LivingEntity;
+
+import java.util.function.Supplier;
+
+public class ConsumeItemAction {
+
+    private static final GOAPKey<Integer> CONSUME_TICK_DURATION = new GOAPKey<>("consume_tick_duration");
+
+    public static <T extends LivingEntity & AVPInventoryHolder> boolean perform(SoundEvent soundEvent, T entity, Blackboard blackboard, Supplier<Boolean> onConsume) {
+        var tickDuration = blackboard.getOrDefault(CONSUME_TICK_DURATION, 0);
+        blackboard.set(CONSUME_TICK_DURATION, tickDuration + 1);
+
+        if (tickDuration >= 32) {
+            return onConsume.get();
+        }
+
+        if (tickDuration % 4 == 0) {
+            // Throttles sound so the sound isn't being spammed.
+            entity.playSound(soundEvent,1F, entity.level().random.nextFloat() * 0.1F + 0.9F);
+        }
+
+        // Not finished, yet.
+        return false;
+
+    }
+}
--- common/src/main/java/com/human/common/gameplay/entity/living/human/marine/ai/action/DrinkPotionAction.java
+++ common/src/main/java/com/human/common/gameplay/entity/living/human/marine/ai/action/DrinkPotionAction.java
@@ -5,8 +5,8 @@ import com.avp.common.model.inventory.AVPInventoryHolder;
 import com.just.goap.GOAPKey;
 import com.just.goap.state.Blackboard;
 import com.just.goap.state.ReadableWorldState;
-import com.lib.common.gameplay.goap.GOAPKeys;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.LivingEntity;
 
 import java.util.List;
@@ -14,23 +14,24 @@ import java.util.Objects;
 
 public class DrinkPotionAction {
 
-    public static <T extends LivingEntity & AVPInventoryHolder> boolean perform(GOAPKey<List<AVPInventory.Entry>> potionEntriesKey, T entity, ReadableWorldState worldState, Blackboard ignored) {
-        // TODO: Fix this once getOrDefault is supported.
-        var potionEntries = worldState.getOrNull(potionEntriesKey);
+    public static <T extends LivingEntity & AVPInventoryHolder> boolean perform(GOAPKey<List<AVPInventory.Entry>> potionEntriesKey, T entity, ReadableWorldState worldState, Blackboard blackboard) {
+        return ConsumeItemAction.perform(SoundEvents.GENERIC_DRINK, entity, blackboard, () -> {
+            // TODO: Fix this once getOrDefault is supported.
+            var potionEntries = worldState.getOrNull(potionEntriesKey);
 
-        if (potionEntries == null || potionEntries.isEmpty()) {
-            // TODO: This is technically a failure, but we return true here to stop the plan.
+            if (potionEntries == null || potionEntries.isEmpty()) {
+                // TODO: This is technically a failure, but we return true here to stop the plan.
+                return true;
+            }
+
+            var potionEntry = potionEntries.getFirst();
+            // Extract the potion contents BEFORE we remove the item from the inventory.
+            var potionContents = Objects.requireNonNull(potionEntry.get(DataComponents.POTION_CONTENTS));
+            // Remove the potion from the entity's inventory.
+            entity.getInventory().removeItem(potionEntry.getItem());
+            // Apply all of the mob effect instances to the marine.
+            potionContents.getAllEffects().forEach(entity::addEffect);
             return true;
-        }
-
-        var potionEntry = potionEntries.getFirst();
-        // Extract the potion contents BEFORE we remove the item from the inventory.
-        var potionContents = Objects.requireNonNull(potionEntry.get(DataComponents.POTION_CONTENTS));
-        // Remove the potion from the entity's inventory.
-        entity.getInventory().removeItem(potionEntry.getItem());
-        // Apply all of the mob effect instances to the marine.
-        potionContents.getAllEffects().forEach(entity::addEffect);
-
-        return true;
+        });
     }
 }
