3a6adc39dd99886815a1e0b38ab3ff97ec224627
bvanseg <bvanseg@gmail.com>
2025-10-09 00:15:33 -0400

    ♻️ Refactor.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/com/human/client/render/item/SentryTurretItemRenderer.java
+++ common/src/main/java/com/human/client/render/item/SentryTurretItemRenderer.java
@@ -10,11 +10,15 @@ public class SentryTurretItemRenderer extends AzItemRenderer {
 
     public static final String NAME = "sentry_turret";
 
-    private static final ResourceLocation GEO = AVPResources.blockGeoModelLocation(NAME);
+    private static final ResourceLocation MODEL = AVPResources.blockGeoModelLocation(NAME);
 
-    private static final ResourceLocation TEX = AVPResources.blockTextureLocation(NAME);
+    private static final ResourceLocation TEXTURE = AVPResources.blockTextureLocation(NAME);
 
     public SentryTurretItemRenderer() {
-        super(AzItemRendererConfig.builder(GEO, TEX).useNewOffset(true).build());
+        super(
+            AzItemRendererConfig.builder(MODEL, TEXTURE)
+                .useNewOffset(true)
+                .build()
+        );
     }
 }
