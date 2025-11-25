50c300fc9d63d8c43b645802fd3bf1b43d31c63b
bvanseg <bvanseg@gmail.com>
2025-11-25 11:14:48 -0500

    ✨ Added BLib.LOGGER.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/com/blib/BLib.java
+++ common/src/main/java/com/blib/BLib.java
@@ -1,6 +1,11 @@
 package com.blib;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BLib {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(BLib.class);
 
     public static BLibMod createMod(String modId) {
         return new BLibMod(modId);
