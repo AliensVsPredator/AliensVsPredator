62737fc882f4ed126eb0b0d09f33918408ffbeb5
bvanseg <bvanseg@gmail.com>
2025-11-26 15:50:53 -0500

    ✨ Added BLibMod#getAllHolders.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/com/blib/BLibMod.java
+++ common/src/main/java/com/blib/BLibMod.java
@@ -11,15 +11,23 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SpawnEggItem;
 import net.minecraft.world.level.block.DispenserBlock;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 
 public class BLibMod {
 
     private final String id;
 
+    private final Map<Registry<?>, List<BLibRegistry<?>>> registryToRegistriesMap;
+
     /* package-private */ BLibMod(String id) {
         this.id = id;
+        this.registryToRegistriesMap = new ConcurrentHashMap<>();
     }
 
     public <T> void addEventListener(BLibEventKey<T> key, Consumer<T> consumer) {
@@ -28,6 +36,16 @@ public class BLibMod {
 
     public <T> BLibRegistry<T> createRegistry(Registry<? super T> registry) {
         var newRegistry = new BLibRegistry<T>(this, registry);
+
+        registryToRegistriesMap.compute(registry, ($, registries) -> {
+            var nonNullRegistries = registries == null
+                ? new ArrayList<BLibRegistry<?>>()
+                : registries;
+
+            nonNullRegistries.add(newRegistry);
+
+            return nonNullRegistries;
+        });
 
         if (registry == BuiltInRegistries.ITEM && Objects.equals("Fabric", BLibServices.MOD_LOADER.getModLoaderName())) {
             newRegistry.addListener(this::autoRegisterDispenserBehavior);
@@ -42,6 +60,17 @@ public class BLibMod {
 
     public ResourceLocation createResourceLocation(String path) {
         return ResourceLocation.fromNamespaceAndPath(id, path);
+    }
+
+    public <T> Collection<BLibHolder<? extends T>> getAllHolders(Registry<T> registry) {
+        @SuppressWarnings("unchecked")
+        var subRegistries = (List<BLibRegistry<T>>) (List<?>) registryToRegistriesMap.getOrDefault(registry, List.of());
+
+        return subRegistries
+            .stream()
+            .map(BLibRegistry::getAll)
+            .flatMap(Collection::stream)
+            .toList();
     }
 
     public String getId() {
