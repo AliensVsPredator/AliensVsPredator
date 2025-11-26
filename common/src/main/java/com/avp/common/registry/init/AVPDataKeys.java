7769f6a2f67b65799acfd2cdf8e4f928f27b50c1
bvanseg <bvanseg@gmail.com>
2025-11-26 02:26:38 -0500

    🧹 Removed alien-related data keys.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/com/avp/common/registry/init/AVPDataKeys.java
+++ common/src/main/java/com/avp/common/registry/init/AVPDataKeys.java
@@ -11,32 +11,6 @@ import com.avp.AVPResources;
 
 public class AVPDataKeys {
 
-    public static final DataKey<Integer> ACID_MULTIPLIER = register(
-        "acid_multiplier",
-        builder -> builder.networkSynchronized(StreamCodecs.INT)
-            .persistent("Multiplier", Codec.INT)
-            .build(1)
-    );
-
-    public static final DataKey<Integer> ACID_TICK_COUNT_FOR_MULTIPLIER = register(
-        "acid_tick_count_for_multiplier",
-        builder -> builder.persistent("TickCountForMultiplier", Codec.INT)
-            .build(0)
-    );
-
-    public static final DataKey<Boolean> ADOLESCENT_HAS_DORSAL_TUBES = register(
-        "adolescent_has_dorsal_tubes",
-        builder -> builder.networkSynchronized(StreamCodecs.BOOLEAN)
-            .build(true)
-    );
-
-    public static final DataKey<Boolean> ALIEN_IS_POISONED = register(
-        "alien_is_poisoned",
-        builder -> builder.networkSynchronized(StreamCodecs.BOOLEAN)
-            .persistent("isPoisoned", Codec.BOOL)
-            .build(false)
-    );
-
     public static final DataKey<Boolean> ENTITY_HAS_WARP_EFFECT = register(
         "entity_has_warp_effect",
         builder -> builder.networkSynchronized(StreamCodecs.BOOLEAN)
@@ -95,71 +69,6 @@ public class AVPDataKeys {
         builder -> builder.networkSynchronized(StreamCodecs.BOOLEAN)
             .persistent("isMale", Codec.BOOL)
             .build(true)
-    );
-
-    public static final DataKey<Integer> OVOMORPH_DESIRE_TO_HATCH = register(
-        "ovomorph_desire_to_hatch",
-        builder -> builder.persistent("desireToHatch", Codec.INT)
-            .build(0)
-    );
-
-    public static final DataKey<Integer> OVOMORPH_HATCH_DURATION_IN_TICKS = register(
-        "ovomorph_hatch_duration_in_ticks",
-        builder -> builder.persistent("hatchDurationInTicks", Codec.INT)
-            .build(-1)
-    );
-
-    // FIXME:
-    // public static final DataKey<Byte> OVOMORPH_HATCH_STATE = register(
-    // "ovomorph_hatch_state",
-    // builder -> builder.networkSynchronized(StreamCodecs.BYTE)
-    // .persistent("hatchState", Codec.BYTE)
-    // .build((byte) Ovomorph.DEFAULT_HATCH_STATE.getId())
-    // );
-
-    public static final DataKey<Boolean> OVOMORPH_IS_ROOTED = register(
-        "ovomorph_is_rooted",
-        builder -> builder.networkSynchronized(StreamCodecs.BOOLEAN)
-            .persistent("isRooted", Codec.BOOL)
-            .build(true)
-    );
-
-    public static final DataKey<Byte> OVOMORPH_MAXIMUM_SPAWN_COUNT = register(
-        "ovomorph_maximum_spawn_count",
-        builder -> builder.networkSynchronized(StreamCodecs.BYTE)
-            .persistent("maximumSpawnCount", Codec.BYTE)
-            .build((byte) 1)
-    );
-
-    public static final DataKey<Integer> OVOMORPH_REMAINING_SPAWN_DELAY_IN_TICKS = register(
-        "ovomorph_remaining_spawn_delay_in_ticks",
-        builder -> builder.persistent("remainingSpawnDelayInTicks", Codec.INT)
-            .build(-1)
-    );
-
-    public static final DataKey<Integer> OVOMORPH_SPAWN_COUNT = register(
-        "ovomorph_spawn_count",
-        builder -> builder.persistent("spawnCount", Codec.INT)
-            .build(0)
-    );
-
-    public static final DataKey<Boolean> PARASITE_IS_FERTILE = register(
-        "parasite_is_fertile",
-        builder -> builder.networkSynchronized(StreamCodecs.BOOLEAN)
-            .persistent("isFertile", Codec.BOOL)
-            .build(true)
-    );
-
-    public static final DataKey<Integer> PARASITE_TICKS_ATTACHED_TO_HOST = register(
-        "parasite_ticks_attached_to_host",
-        builder -> builder.persistent("ticksAttachedToHost", Codec.INT)
-            .build(0)
-    );
-
-    public static final DataKey<Boolean> XENOMORPH_IS_CRAWLING = register(
-        "xenomorph_is_crawling",
-        builder -> builder.networkSynchronized(StreamCodecs.BOOLEAN)
-            .build(false)
     );
 
     private static <T> DataKey<T> register(String id, Function<DataKey.Builder<T>, DataKey<T>> factory) {
