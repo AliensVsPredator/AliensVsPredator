f99619be303ca72b3373cb6879e94e434e6da2af
bvanseg <bvanseg@gmail.com>
2025-12-05 11:14:58 -0500

    ♻️ Added back common item tags.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/com/compat/CommonItemTags.java
+++ common/src/main/java/com/compat/CommonItemTags.java
@@ -14,17 +14,27 @@ public class CommonItemTags {
 
     public static final TagKey<Item> GEMS_DIAMOND = create("gems/diamond");
 
+    public static final TagKey<Item> HEAVY_METAL = create("ingots/heavy_metal");
+
+    public static final TagKey<Item> HIDDEN_FROM_RECIPE_VIEWERS = create("hidden_from_recipe_viewers");
+
     public static final TagKey<Item> INGOTS = create("ingots");
 
     public static final TagKey<Item> INGOTS_COPPER = create("ingots/copper");
 
     public static final TagKey<Item> INGOTS_GOLD = create("ingots/gold");
 
+    public static final TagKey<Item> LEATHERS = create("leathers");
+
+    public static final TagKey<Item> MUSIC_DISCS = create("music_discs");
+
     public static final TagKey<Item> NUGGETS = create("nuggets");
 
     public static final TagKey<Item> NUGGETS_GOLD = create("nuggets/gold");
 
     public static final TagKey<Item> NUGGETS_IRON = create("nuggets/iron");
+
+    public static final TagKey<Item> ORES = create("ores");
 
     public static final TagKey<Item> RAW_MATERIALS = create("raw_materials");
 
@@ -33,6 +43,8 @@ public class CommonItemTags {
     public static final TagKey<Item> RAW_MATERIALS_IRON = create("raw_materials/iron");
 
     public static final TagKey<Item> RODS_WOODEN = create("rods/wooden");
+
+    public static final TagKey<Item> STORAGE_BLOCKS = create("storage_blocks");
 
     public static final TagKey<Item> STRINGS = create("strings");
 
