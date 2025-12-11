d66e6e74c86096b3c2ce93d0ea69dbdcd8e0f39c
bvanseg <bvanseg@gmail.com>
2025-12-11 12:26:46 -0500

    ✨ Added BLibRegistries.
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ common/src/main/java/com/blib/internal/registry/BLibRegistries.java
@@ -0,0 +1,34 @@
+package com.blib.internal.registry;
+
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+
+import java.util.List;
+
+public class BLibRegistries {
+
+    public static final List<Registry<?>> REGISTRATION_ORDER = List.of(
+        // Independent registries.
+        BuiltInRegistries.BLOCK,
+        BuiltInRegistries.DATA_COMPONENT_TYPE,
+        BuiltInRegistries.DECORATED_POT_PATTERN,
+        BuiltInRegistries.ENTITY_TYPE,
+        BuiltInRegistries.GAME_EVENT,
+        BuiltInRegistries.MENU,
+        BuiltInRegistries.MOB_EFFECT,
+        BuiltInRegistries.PARTICLE_TYPE,
+        BuiltInRegistries.POINT_OF_INTEREST_TYPE,
+        BuiltInRegistries.RECIPE_SERIALIZER,
+        BuiltInRegistries.RECIPE_TYPE,
+        BuiltInRegistries.SOUND_EVENT,
+        BuiltInRegistries.VILLAGER_PROFESSION,
+        // Depends on sound events.
+        BuiltInRegistries.ARMOR_MATERIAL,
+        // Depends on blocks.
+        BuiltInRegistries.BLOCK_ENTITY_TYPE,
+        // Potentially depends on blocks (block items), entities (spawn egg items) or armor materials (armor items).
+        BuiltInRegistries.ITEM,
+        // Depends on blocks and items.
+        BuiltInRegistries.CREATIVE_MODE_TAB
+    );
+}
--- neoforge/src/main/java/com/blib/neoforge/service/impl/NeoForgeBLibModContainer.java
+++ neoforge/src/main/java/com/blib/neoforge/service/impl/NeoForgeBLibModContainer.java
@@ -2,9 +2,9 @@ package com.blib.neoforge.service.impl;
 
 import com.blib.BLibHolder;
 import com.blib.BLibMod;
+import com.blib.internal.registry.BLibRegistries;
 import com.just.core.functional.tuple.Tuple2;
 import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
@@ -19,7 +19,6 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 class NeoForgeBLibModContainer {
 
@@ -35,25 +34,14 @@ class NeoForgeBLibModContainer {
 
     public NeoForgeBLibModContainer(BLibMod mod) {
         this.mod = mod;
-        this.registryToDeferredRegisterMap = Stream.of(
-            BuiltInRegistries.ARMOR_MATERIAL,
-            BuiltInRegistries.BLOCK,
-            BuiltInRegistries.BLOCK_ENTITY_TYPE,
-            BuiltInRegistries.CREATIVE_MODE_TAB,
-            BuiltInRegistries.DATA_COMPONENT_TYPE,
-            BuiltInRegistries.DECORATED_POT_PATTERN,
-            BuiltInRegistries.ENTITY_TYPE,
-            BuiltInRegistries.GAME_EVENT,
-            BuiltInRegistries.ITEM,
-            BuiltInRegistries.MENU,
-            BuiltInRegistries.MOB_EFFECT,
-            BuiltInRegistries.PARTICLE_TYPE,
-            BuiltInRegistries.POINT_OF_INTEREST_TYPE,
-            BuiltInRegistries.RECIPE_SERIALIZER,
-            BuiltInRegistries.RECIPE_TYPE,
-            BuiltInRegistries.SOUND_EVENT,
-            BuiltInRegistries.VILLAGER_PROFESSION
-        ).collect(Collectors.toMap(Function.identity(), registry -> createDeferredRegistry(mod.getId(), registry)));
+        this.registryToDeferredRegisterMap = BLibRegistries.REGISTRATION_ORDER
+            .stream()
+            .collect(
+                Collectors.toMap(
+                    Function.identity(),
+                    registry -> createDeferredRegistry(mod.getId(), registry)
+                )
+            );
 
         this.entityAttributeSupplierPairs = new ArrayList<>();
     }
