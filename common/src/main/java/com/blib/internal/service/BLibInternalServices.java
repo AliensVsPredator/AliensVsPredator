140a2c600a1f5129f7dd479db9fca55a5ad716cd
bvanseg <bvanseg@gmail.com>
2025-12-11 21:48:32 -0500

    ♻️ Refactor.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/com/blib/internal/service/BLibInternalServices.java
+++ common/src/main/java/com/blib/internal/service/BLibInternalServices.java
@@ -1,10 +1,24 @@
 package com.blib.internal.service;
 
+import com.blib.BLib;
+
+import java.util.ServiceLoader;
+
 public class BLibInternalServices {
 
-    public static final BLibModService MOD = BLibServiceLoader.load(BLibModService.class);
+    public static final BLibModService MOD = load(BLibModService.class);
 
-    public static final BLibModLoaderService MOD_LOADER = BLibServiceLoader.load(BLibModLoaderService.class);
+    public static final BLibModLoaderService MOD_LOADER = load(BLibModLoaderService.class);
 
-    public static final BLibRegistryService REGISTRY = BLibServiceLoader.load(BLibRegistryService.class);
+    public static final BLibRegistryService REGISTRY = load(BLibRegistryService.class);
+
+    public static <T> T load(Class<T> clazz) {
+        var loadedService = ServiceLoader.load(clazz)
+            .findFirst()
+            .orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
+
+        BLib.LOGGER.debug("Loaded {} for service {}", loadedService, clazz);
+
+        return loadedService;
+    }
 }
--- common/src/main/java/com/blib/service/BLibServices.java
+++ common/src/main/java/com/blib/service/BLibServices.java
@@ -1,17 +1,28 @@
 package com.blib.service;
 
-import com.blib.internal.service.BLibServiceLoader;
+import com.blib.BLib;
+
+import java.util.ServiceLoader;
 
 public class BLibServices {
 
-    public static final BLibClientNetworkingService CLIENT_NETWORKING = BLibServiceLoader.load(BLibClientNetworkingService.class);
+    public static final BLibClientNetworkingService CLIENT_NETWORKING = load(BLibClientNetworkingService.class);
 
-    public static final BLibClientRegistryService CLIENT_REGISTRY = BLibServiceLoader.load(BLibClientRegistryService.class);
+    public static final BLibClientRegistryService CLIENT_REGISTRY = load(BLibClientRegistryService.class);
 
-    public static final BLibEventService EVENT = BLibServiceLoader.load(BLibEventService.class);
+    public static final BLibEventService EVENT = load(BLibEventService.class);
 
-    public static final BLibFactoryService FACTORY = BLibServiceLoader.load(BLibFactoryService.class);
+    public static final BLibFactoryService FACTORY = load(BLibFactoryService.class);
 
-    public static final BLibServerNetworkingService SERVER_NETWORKING = BLibServiceLoader.load(BLibServerNetworkingService.class);
+    public static final BLibServerNetworkingService SERVER_NETWORKING = load(BLibServerNetworkingService.class);
 
+    public static <T> T load(Class<T> clazz) {
+        var loadedService = ServiceLoader.load(clazz)
+            .findFirst()
+            .orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
+
+        BLib.LOGGER.debug("Loaded {} for service {}", loadedService, clazz);
+
+        return loadedService;
+    }
 }
