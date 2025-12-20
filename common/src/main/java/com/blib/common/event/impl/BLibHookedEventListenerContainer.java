50e6c4dd0ef70bec47f6264b6185d99a6618b9c5
bvanseg <bvanseg@gmail.com>
2025-12-19 21:15:30 -0500

    ✨ Added BLibHookedEventListenerContainer.
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ common/src/main/java/com/blib/common/event/impl/BLibHookedEventListenerContainer.java
@@ -0,0 +1,25 @@
+package com.blib.common.event.impl;
+
+import com.blib.common.exception.BLibModInitializationException;
+import com.blib.common.model.BLibModState;
+import com.blib.common.model.access.BLibModStateAccess;
+
+public class BLibHookedEventListenerContainer<T extends BLibModStateAccess, Dispatcher> extends BLibEventListenerContainer<Dispatcher> {
+
+    private final T modStateAccess;
+
+    public BLibHookedEventListenerContainer(T modStateAccess) {
+        this.modStateAccess = modStateAccess;
+    }
+
+    @Override
+    public void register(Dispatcher dispatcher) {
+        if (modStateAccess.state() != BLibModState.UNINITIALIZED) {
+            throw new BLibModInitializationException(
+                "Attempted to register an event outside of mod's initialization window. Mod State: %s".formatted(modStateAccess.state())
+            );
+        }
+
+        super.register(dispatcher);
+    }
+}
