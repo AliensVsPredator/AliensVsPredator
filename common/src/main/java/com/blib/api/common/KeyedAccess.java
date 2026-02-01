981fa491507a2b73189c100b9439e8f310f026c5
bvanseg <bvanseg@gmail.com>
2026-02-01 13:02:05 -0500

    ✨ Added KeyedAccess.
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ common/src/main/java/com/blib/api/common/KeyedAccess.java
@@ -0,0 +1,42 @@
+package com.blib.api.common;
+
+import com.just.core.functional.option.Option;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+@FunctionalInterface
+public interface KeyedAccess<K, V> {
+
+    @Nullable
+    V getOrNull(K key);
+
+    default Option<V> get(K key) {
+        return Option.ofNullable(getOrNull(key));
+    }
+
+    default V getOrDefault(K key, V defaultValue) {
+        var value = getOrNull(key);
+
+        return value == null
+            ? defaultValue
+            : value;
+    }
+
+    default V getOrElse(K key, Supplier<V> defaultValueSupplier) {
+        var value = getOrNull(key);
+
+        return value == null
+            ? defaultValueSupplier.get()
+            : value;
+    }
+
+    default V getOrThrow(K key) {
+        return Objects.requireNonNull(getOrNull(key));
+    }
+
+    default boolean has(K key) {
+        return getOrNull(key) != null;
+    }
+}
