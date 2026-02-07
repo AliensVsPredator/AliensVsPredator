88452ad1f55d7f7626305a761bfe3076e489bbce
bvanseg <bvanseg@gmail.com>
2026-02-07 18:10:17 -0500

    ♻️ Automatically save dirty registered property containers during server save.
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ common/src/main/java/com/blib/internal/common/property/BLibPropertyContainerSaveHandler.java
@@ -0,0 +1,34 @@
+package com.blib.internal.common.property;
+
+import net.minecraft.server.MinecraftServer;
+import org.jetbrains.annotations.ApiStatus;
+
+import com.blib.api.common.property.v1.BLibProperties;
+import com.blib.api.common.registry.v1.BLibBuiltInRegistries;
+import com.blib.mod.BLib;
+
+@ApiStatus.Internal
+public final class BLibPropertyContainerSaveHandler {
+
+    public static final BLibPropertyContainerSaveHandler INSTANCE = new BLibPropertyContainerSaveHandler();
+
+    private BLibPropertyContainerSaveHandler() {}
+
+    public void save(MinecraftServer server) {
+        for (var entry : BLibBuiltInRegistries.PROPERTY_CONTAINER_TYPES) {
+            var container = entry.container();
+
+            if (!container.isDirty()) {
+                continue;
+            }
+
+            var result = BLibProperties.save(entry.filePath(), container, container.getSchema());
+
+            if (result.isErr()) {
+                BLib.LOGGER.error("Failed to save property container: {}", result.unwrapErr());
+            }
+
+            container.clearDirty();
+        }
+    }
+}
