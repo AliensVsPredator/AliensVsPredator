075d6f58a638ef55a503d955ce1c5da8a16fed1b
bvanseg <bvanseg@gmail.com>
2026-02-08 23:52:57 -0500

    ✨ Added BLibNetworkAccess#sendToAllClientsTrackingChunk and BLibNetworkAccess#sendToAllClientsTrackingEntity.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/com/blib/api/common/mod/v1/model/access/BLibNetworkAccess.java
+++ common/src/main/java/com/blib/api/common/mod/v1/model/access/BLibNetworkAccess.java
@@ -1,8 +1,11 @@
 package com.blib.api.common.mod.v1.model.access;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
 import org.jetbrains.annotations.ApiStatus;
 
 import com.blib.api.common.mod.v1.BLibMod;
@@ -28,5 +31,13 @@ public class BLibNetworkAccess {
 
     public void sendToAllClients(MinecraftServer minecraftServer, CustomPacketPayload customPacketPayload) {
         BLibInternalServices.SERVER_NETWORKING.sendToAllClients(minecraftServer, customPacketPayload);
+    }
+
+    public void sendToAllClientsTrackingChunk(ServerLevel serverLevel, BlockPos blockPos, CustomPacketPayload customPacketPayload) {
+        BLibInternalServices.SERVER_NETWORKING.sendToAllClientsTrackingChunk(serverLevel, blockPos, customPacketPayload);
+    }
+
+    public void sendToAllClientsTrackingEntity(Entity entity, CustomPacketPayload customPacketPayload) {
+        BLibInternalServices.SERVER_NETWORKING.sendToAllClientsTrackingEntity(entity, customPacketPayload);
     }
 }
