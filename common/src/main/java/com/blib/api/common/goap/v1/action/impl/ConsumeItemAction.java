2083b8b47ee0ce273e54302f2a573df8a23771d5
bvanseg <bvanseg@gmail.com>
2026-02-11 22:42:39 -0500

    ✨ Added ConsumeItemAction.
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ common/src/main/java/com/blib/api/common/goap/v1/action/impl/ConsumeItemAction.java
@@ -0,0 +1,38 @@
+package com.blib.api.common.goap.v1.action.impl;
+
+import com.just.goap.StateKey;
+import com.just.goap.action.Action;
+import com.just.goap.state.Blackboard;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.entity.LivingEntity;
+
+import java.util.function.Supplier;
+
+public class ConsumeItemAction {
+
+    private static final StateKey<Integer> CONSUME_TICK_DURATION = StateKey.sensed("consume_tick_duration");
+
+    public static Action.Signal perform(
+        SoundEvent soundEvent,
+        LivingEntity entity,
+        Blackboard blackboard,
+        Supplier<Action.Signal> onConsume
+    ) {
+        var tickDuration = blackboard.getOrDefault(CONSUME_TICK_DURATION, 0);
+        blackboard.set(CONSUME_TICK_DURATION, tickDuration + 1);
+
+        if (tickDuration >= 32) {
+            // Reset consume tick duration.
+            blackboard.set(CONSUME_TICK_DURATION, 0);
+            return onConsume.get();
+        }
+
+        if (tickDuration % 4 == 0) {
+            // Throttles sound so the sound isn't being spammed.
+            entity.playSound(soundEvent, 1F, entity.level().random.nextFloat() * 0.1F + 0.9F);
+        }
+
+        // Not finished, yet.
+        return Action.Signal.CONTINUE;
+    }
+}
